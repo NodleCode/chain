c04e0b6b4f888d0462762047f7857c26c2372b00
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-01-07 13:47:11 -0800

    swap a few metadata

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ pub use substrate_cli::{VersionInfo, IntoExit, error};
 
 fn run() -> cli::error::Result<()> {
 	let version = VersionInfo {
-		name: "Substrate Node",
+		name: "Nodle Node",
 		commit: env!("VERGEN_SHA_SHORT"),
 		version: env!("CARGO_PKG_VERSION"),
 		executable_name: "nodle-chain",
