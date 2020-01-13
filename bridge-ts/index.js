92246394430e89221c3c55237255d3fea482b31f
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-01-13 11:02:02 -0800

    host selection

--- bridge-ts/index.js
+++ bridge-ts/index.js
@@ -6,9 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
 var port = 8080;
+var host = "localhost";
 app.get("/", function (req, res) {
     res.send("Nodle Chain bridge");
 });
-app.listen(port, function () {
-    console.log("Listening on " + port + "...");
+app.listen(port, host, function () {
+    console.log("Listening on http://" + host + ":" + port + "...");
 });
--- bridge-ts/index.ts
+++ bridge-ts/index.ts
@@ -2,11 +2,12 @@ import express from "express"
 
 const app = express()
 const port = 8080
+const host = "localhost"
 
 app.get("/", (req, res) => {
 	res.send("Nodle Chain bridge")
 })
 
-app.listen(port, () => {
-	console.log(`Listening on ${port}...`);
+app.listen(port, host, () => {
+	console.log(`Listening on http://${host}:${port}...`);
 })
