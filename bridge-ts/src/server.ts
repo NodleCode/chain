873edb3ff46238347b0674210e9f257813fede1f
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-01-13 11:42:18 -0800

    oracle reward submission

--- bridge-ts/src/server.ts
+++ bridge-ts/src/server.ts
@@ -1,4 +1,6 @@
 import express from "express"
+import bodyParser from "body-parser"
+
 import { Keyring } from '@polkadot/api'
 import { cryptoWaitReady } from '@polkadot/util-crypto'
 
@@ -10,6 +12,9 @@ const hasOracleSk = oracleSk.length > 0
 
 export async function startServer(chainApi: any) {
 	const app = express()
+	app.use(bodyParser.urlencoded({ extended: true }))
+	app.use(bodyParser.json())
+	app.use(bodyParser.raw())
 
 	// Oracle only calls
 	if (hasOracleSk) {
@@ -19,8 +24,16 @@ export async function startServer(chainApi: any) {
 
 		console.log(`Oracle has address ${oraclePair.address}`)
 
-		app.post("/oracle/reward", (req, res) => {
+		app.post("/oracle/reward", async (req, res) => {
+			console.log(`Got reward ${JSON.stringify(req.body)}`)
+			const reward = chainApi.tx.allocations.submitReward(
+				req.body.rootHash,
+				req.body.destination,
+				req.body.amount
+			)
+			const hash = await reward.signAndSend(oraclePair)
 
+			res.send(hash)
 		})
 	}
 	
