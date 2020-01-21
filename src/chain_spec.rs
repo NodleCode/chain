d18245857e429e77ea557f9192833cd50b13770e
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-01-20 16:51:52 -0800

    build nergal testnet

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -1,4 +1,4 @@
-use sp_core::{Pair, Public, sr25519};
+use sp_core::{crypto::Ss58Codec, ed25519, Pair, Public, sr25519};
 use nodle_chain_runtime::{
 	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
 	IndicesConfig, SystemConfig, WASM_BINARY, Signature, TechnicalMembershipConfig,
@@ -22,8 +22,8 @@ pub type ChainSpec = sc_service::ChainSpec<GenesisConfig>;
 pub enum Alternative {
 	/// Whatever the current runtime is, with just Alice as an auth.
 	Development,
-	/// Whatever the current runtime is, with simple Alice/Bob auths.
-	LocalTestnet,
+
+	Nergal,
 }
 
 /// Helper function to generate a crypto pair from seed
@@ -33,6 +33,11 @@ pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Pu
 		.public()
 }
 
+/// Helper function to generate a crypto pair from an address
+pub fn get_from_addr<TPublic: Public>(addr: &str) -> TPublic {
+	TPublic::from_string(addr).expect("static values are valid; qed")
+}
+
 type AccountPublic = <Signature as Verify>::Signer;
 
 /// Helper function to generate an account ID from seed
@@ -40,6 +45,13 @@ pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
 	AccountPublic: From<<TPublic::Pair as Pair>::Public>
 {
 	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+}
+
+/// Helper function to generate an account ID from an address
+pub fn get_account_id_from_addr<TPublic: Public>(addr: &str) -> AccountId where
+	AccountPublic: From<TPublic>
+{
+	AccountPublic::from(get_from_addr::<TPublic>(addr)).into_account()
 }
 
 /// Helper function to generate an authority key for Aura
@@ -82,34 +94,26 @@ impl Alternative {
 				None,
 				None
 			),
-			Alternative::LocalTestnet => ChainSpec::from_genesis(
-				"Local Testnet",
-				"local_testnet",
+			Alternative::Nergal => ChainSpec::from_genesis(
+				"Nergal",
+				"nergal",
 				|| testnet_genesis(vec![
-					get_authority_keys_from_seed("Alice"),
-					get_authority_keys_from_seed("Bob"),
+					(
+						get_from_addr::<AuraId>("5CFUAAW7umifCuzdvH5KKjWHM5dKP2ADnL54b9fbjECpgXEM"),
+						get_from_addr::<GrandpaId>("5CLAMY86UHqR4DYFL1ss3mPQJPJVBHxRyNFKHwwitNjK2KRM")
+					)
 				],
 				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie")
+					get_account_id_from_addr::<sr25519::Public>("5F6sSf67EXUcqxsZcjx9A5hgTmXFpHA4F2XJa65Q8ZmWVPXd")
 				],
 				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie"),
-					get_account_id_from_seed::<sr25519::Public>("Dave"),
-					get_account_id_from_seed::<sr25519::Public>("Eve"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+					get_account_id_from_addr::<sr25519::Public>("5F6sSf67EXUcqxsZcjx9A5hgTmXFpHA4F2XJa65Q8ZmWVPXd"), // Root
+					get_account_id_from_addr::<ed25519::Public>("5CLAMY86UHqR4DYFL1ss3mPQJPJVBHxRyNFKHwwitNjK2KRM"), // Validator stash
+					get_account_id_from_addr::<sr25519::Public>("5CFUAAW7umifCuzdvH5KKjWHM5dKP2ADnL54b9fbjECpgXEM"), // Validtor hot wallet
+					get_account_id_from_addr::<sr25519::Public>("5EaAaHrJaegsiSNgKJch1QjwCE8DFXRKxxg2tdVQhBHTWsC5")  // Oracle
 				],
 				vec![
-					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+					get_account_id_from_addr::<sr25519::Public>("5EaAaHrJaegsiSNgKJch1QjwCE8DFXRKxxg2tdVQhBHTWsC5")
 				],
 				true),
 				vec![],
@@ -124,7 +128,7 @@ impl Alternative {
 	pub(crate) fn from(s: &str) -> Option<Self> {
 		match s {
 			"dev" => Some(Alternative::Development),
-			"" | "local" => Some(Alternative::LocalTestnet),
+			"" | "nergal" => Some(Alternative::Nergal),
 			_ => None,
 		}
 	}
