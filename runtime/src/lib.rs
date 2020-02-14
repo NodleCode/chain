329d834c509254ec86156f59a83c593184b81c48
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-02-14 12:43:26 -0800

    bump runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -52,7 +52,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     impl_name: create_runtime_str!("nodle-chain"),
     authoring_version: 1,
     spec_version: 1,
-    impl_version: 0,
+    impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
 };
 
