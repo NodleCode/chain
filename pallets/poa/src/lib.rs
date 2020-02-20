b0c8460b39f2f46137f465e99aab5a8f91a68396
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-02-20 09:31:09 -0800

    split tests away from lib.rs files

--- pallets/poa/src/lib.rs
+++ pallets/poa/src/lib.rs
@@ -3,6 +3,9 @@
 //! A runtime module to handle help managing validators through the `membership`,
 //! support the deletion and addition of validators by a root authority n.
 
+#[cfg(test)]
+mod tests;
+
 use frame_support::traits::{ChangeMembers, InitializeMembers};
 use frame_support::{decl_module, decl_storage};
 use session::SessionManager;
@@ -76,133 +79,5 @@ impl<T: Trait> session::historical::SessionManager<T::AccountId, FullIdentificat
     }
     fn end_session(end_index: SessionIndex) {
         <Self as session::SessionManager<_>>::end_session(end_index)
-    }
-}
-
-/// tests for this module
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
-    use sp_core::{crypto::key_types, H256};
-    use sp_runtime::{
-        testing::{Header, UintAuthorityId},
-        traits::{BlakeTwo256, ConvertInto, IdentityLookup, OpaqueKeys},
-        KeyTypeId, Perbill,
-    };
-
-    impl_outer_origin! {
-        pub enum Origin for Test {}
-    }
-
-    // For testing the module, we construct most of a mock runtime. This means
-    // first constructing a configuration type (`Test`) which `impl`s each of the
-    // configuration traits of modules we want to use.
-    #[derive(Clone, Eq, PartialEq)]
-    pub struct Test;
-    parameter_types! {
-        pub const BlockHashCount: u64 = 250;
-        pub const MaximumBlockWeight: Weight = 1024;
-        pub const MaximumBlockLength: u32 = 2 * 1024;
-        pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
-    }
-    impl system::Trait for Test {
-        type Origin = Origin;
-        type Call = ();
-        type Index = u64;
-        type BlockNumber = u64;
-        type Hash = H256;
-        type Hashing = BlakeTwo256;
-        type AccountId = u64;
-        type Lookup = IdentityLookup<Self::AccountId>;
-        type Header = Header;
-        type Event = ();
-        type BlockHashCount = BlockHashCount;
-        type MaximumBlockWeight = MaximumBlockWeight;
-        type MaximumBlockLength = MaximumBlockLength;
-        type AvailableBlockRatio = AvailableBlockRatio;
-        type Version = ();
-        type ModuleToIndex = ();
-    }
-    parameter_types! {
-        pub const DisabledValidatorsThreshold: Perbill = Perbill::from_percent(33);
-    }
-    pub type AuthorityId = u64;
-    pub struct TestSessionHandler;
-    impl session::SessionHandler<AuthorityId> for TestSessionHandler {
-        const KEY_TYPE_IDS: &'static [KeyTypeId] = &[key_types::DUMMY];
-
-        fn on_new_session<Ks: OpaqueKeys>(
-            _changed: bool,
-            _validators: &[(AuthorityId, Ks)],
-            _queued_validators: &[(AuthorityId, Ks)],
-        ) {
-        }
-
-        fn on_disabled(_validator_index: usize) {}
-
-        fn on_genesis_session<Ks: OpaqueKeys>(_validators: &[(AuthorityId, Ks)]) {}
-    }
-    impl session::ShouldEndSession<u64> for TestSessionHandler {
-        fn should_end_session(_now: u64) -> bool {
-            false
-        }
-    }
-    impl session::Trait for Test {
-        type SessionManager = Module<Test>;
-        type SessionHandler = TestSessionHandler;
-        type ShouldEndSession = TestSessionHandler;
-        type Event = ();
-        type Keys = UintAuthorityId;
-        type ValidatorId = <Test as system::Trait>::AccountId;
-        type ValidatorIdOf = ConvertInto;
-        type DisabledValidatorsThreshold = DisabledValidatorsThreshold;
-    }
-    impl Trait for Test {}
-
-    type SessionModule = session::Module<Test>;
-    type TestModule = Module<Test>;
-
-    pub const VALIDATOR: u64 = 1;
-
-    // This function basically just builds a genesis storage key/value store according to
-    // our desired mockup.
-    fn new_test_ext() -> sp_io::TestExternalities {
-        system::GenesisConfig::default()
-            .build_storage::<Test>()
-            .unwrap()
-            .into()
-    }
-
-    #[test]
-    fn validators_update_propagate() {
-        new_test_ext().execute_with(|| {
-            TestModule::change_members_sorted(&[], &[], &[VALIDATOR]);
-
-            SessionModule::rotate_session();
-            let queued_keys = SessionModule::queued_keys();
-            assert_eq!(queued_keys.len(), 1);
-            assert_eq!(queued_keys[0].0, VALIDATOR);
-
-            SessionModule::rotate_session();
-            assert_eq!(SessionModule::validators(), vec![VALIDATOR]);
-        })
-    }
-
-    #[test]
-    fn change_members_sorted() {
-        new_test_ext().execute_with(|| {
-            TestModule::change_members_sorted(&[], &[], &[VALIDATOR]);
-            assert_eq!(TestModule::new_session(0), Some(vec![VALIDATOR]));
-        })
-    }
-
-    #[test]
-    fn new_session_return_members() {
-        new_test_ext().execute_with(|| {
-            TestModule::initialize_members(&[VALIDATOR]);
-            assert_eq!(TestModule::new_session(0), Some(vec![VALIDATOR]));
-        })
     }
 }
--- pallets/reserve/src/lib.rs
+++ pallets/reserve/src/lib.rs
@@ -3,6 +3,9 @@
 //! A module that is called by the `collective` and is in charge of holding
 //! the company funds.
 
+#[cfg(test)]
+mod tests;
+
 use frame_support::{
     decl_event, decl_module, decl_storage,
     dispatch::DispatchResult,
@@ -100,126 +103,5 @@ impl<T: Trait> OnUnbalanced<NegativeImbalanceOf<T>> for Module<T> {
         let _ = T::Currency::resolve_creating(&Self::account_id(), amount);
 
         Self::deposit_event(RawEvent::Deposit(numeric_amount));
-    }
-}
-
-/// tests for this module
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use frame_support::{
-        assert_noop, assert_ok, impl_outer_origin, ord_parameter_types, parameter_types,
-        traits::Imbalance, weights::Weight,
-    };
-    use sp_core::H256;
-    use sp_runtime::{
-        testing::Header,
-        traits::{BlakeTwo256, IdentityLookup},
-        DispatchError::BadOrigin,
-        Perbill,
-    };
-    use system::EnsureSignedBy;
-
-    impl_outer_origin! {
-        pub enum Origin for Test {}
-    }
-
-    // For testing the module, we construct most of a mock runtime. This means
-    // first constructing a configuration type (`Test`) which `impl`s each of the
-    // configuration traits of modules we want to use.
-    #[derive(Clone, Eq, PartialEq)]
-    pub struct Test;
-    parameter_types! {
-        pub const BlockHashCount: u64 = 250;
-        pub const MaximumBlockWeight: Weight = 1024;
-        pub const MaximumBlockLength: u32 = 2 * 1024;
-        pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
-    }
-    impl system::Trait for Test {
-        type Origin = Origin;
-        type Call = ();
-        type Index = u64;
-        type BlockNumber = u64;
-        type Hash = H256;
-        type Hashing = BlakeTwo256;
-        type AccountId = u64;
-        type Lookup = IdentityLookup<Self::AccountId>;
-        type Header = Header;
-        type Event = ();
-        type BlockHashCount = BlockHashCount;
-        type MaximumBlockWeight = MaximumBlockWeight;
-        type MaximumBlockLength = MaximumBlockLength;
-        type AvailableBlockRatio = AvailableBlockRatio;
-        type Version = ();
-        type ModuleToIndex = ();
-    }
-    impl balances::Trait for Test {
-        type Balance = u64;
-        type OnNewAccount = ();
-        type OnReapAccount = ();
-        type Event = ();
-        type TransferPayment = ();
-        type DustRemoval = ();
-        type ExistentialDeposit = ();
-        type CreationFee = ();
-    }
-
-    ord_parameter_types! {
-        pub const Admin: u64 = 1;
-    }
-    impl Trait for Test {
-        type Event = ();
-        type Currency = balances::Module<Self>;
-        type ExternalOrigin = EnsureSignedBy<Admin, u64>;
-    }
-    type TestModule = Module<Test>;
-    type Balances = balances::Module<Test>;
-
-    type PositiveImbalanceOf<T> =
-        <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::PositiveImbalance;
-
-    // This function basically just builds a genesis storage key/value store according to
-    // our desired mockup.
-    fn new_test_ext() -> sp_io::TestExternalities {
-        system::GenesisConfig::default()
-            .build_storage::<Test>()
-            .unwrap()
-            .into()
-    }
-
-    #[test]
-    fn spend_error_if_bad_origin() {
-        new_test_ext().execute_with(|| {
-            assert_noop!(TestModule::spend(Origin::signed(0), 1, 1), BadOrigin);
-        })
-    }
-
-    #[test]
-    fn spend_funds_to_target() {
-        new_test_ext().execute_with(|| {
-            let mut total_imbalance = <PositiveImbalanceOf<Test>>::zero();
-            let r = <Test as Trait>::Currency::deposit_creating(&TestModule::account_id(), 100);
-            total_imbalance.subsume(r);
-
-            assert_eq!(Balances::free_balance(TestModule::account_id()), 100);
-            assert_eq!(Balances::free_balance(3), 0);
-            assert_ok!(TestModule::spend(Origin::signed(Admin::get()), 3, 100));
-            assert_eq!(Balances::free_balance(3), 100);
-            assert_eq!(Balances::free_balance(TestModule::account_id()), 0);
-        })
-    }
-
-    #[test]
-    fn tip() {
-        new_test_ext().execute_with(|| {
-            let mut total_imbalance = <PositiveImbalanceOf<Test>>::zero();
-            let r = <Test as Trait>::Currency::deposit_creating(&999, 100);
-            total_imbalance.subsume(r);
-
-            assert_ok!(TestModule::tip(Origin::signed(999), 50));
-            assert_eq!(Balances::free_balance(999), 50);
-            assert_eq!(Balances::free_balance(TestModule::account_id()), 50);
-        })
     }
 }
