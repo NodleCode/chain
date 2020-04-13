c3a261b0dcbd0d2b34b3be4d169a5eda7cd3409e
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-04-13 07:58:18 -0700

    add gplv3 header to the source code files

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the Nodle Chain distributed at https://github.com/NodleCode/chain
+ * Copyright (C) 2020  Nodle International
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 use crate::Balance;
 use crate::BlockNumber;
 pub use sp_runtime::Perbill;
