4dda78e1041c7cf2b69df524b1cf67ba7c960d30
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-05-06 13:30:58 -0700

    enable root-of-trust rpc

--- node/src/service.rs
+++ node/src/service.rs
@@ -54,6 +54,8 @@ macro_rules! new_full_start {
     ($config:expr) => {{
         use std::sync::Arc;
 
+        type RpcExtension = jsonrpc_core::IoHandler<sc_rpc::Metadata>;
+
         let mut import_setup = None;
         let inherent_data_providers = sp_inherents::InherentDataProviders::new();
 
@@ -98,6 +100,14 @@ macro_rules! new_full_start {
 
             import_setup = Some((block_import, grandpa_link, babe_link));
             Ok(import_queue)
+        })?
+        .with_rpc_extensions(|builder| -> std::result::Result<RpcExtension, _> {
+            let mut io = jsonrpc_core::IoHandler::default();
+            io.extend_with(pallet_root_of_trust_rpc::RootOfTrustApi::to_delegate(
+                pallet_root_of_trust_rpc::RootOfTrust::new(builder.client().clone()),
+            ));
+
+            Ok(io)
         })?;
 
         (builder, import_setup, inherent_data_providers)
