5073b00ca234750393a0ec55751a6595a8add323
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-05-22 11:36:24 -0700

    swap hashing library

--- misc/bridge/functions/src/index.ts
+++ misc/bridge/functions/src/index.ts
@@ -1,16 +1,27 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import Blake2s from 'blake2s-js';
+import * as blake2 from 'blake2';
 
 admin.initializeApp();
+
+const byteToHexString = (uint8arr: Uint8Array) => {
+    var hexStr = '';
+    for (var i = 0; i < uint8arr.length; i++) {
+        var hex = (uint8arr[i] & 0xff).toString(16);
+        hex = (hex.length === 1) ? '0' + hex : hex;
+        hexStr += hex;
+    }
+
+    return hexStr.toUpperCase();
+}
 
 export const registerMemo = functions.https.onRequest(async (request, response) => {
     const nodlePublicKey = request.body.ss58Address;
 
     // In order to generate a deterministic ID we hash the user's address.
-    const hasher = new Blake2s(32);
-    hasher.update(new TextEncoder().encode(nodlePublicKey));
-    const memoHash = hasher.hexDigest();
+    var hasher = blake2.createHash('blake2b', { digestLength: 16 });
+    hasher.update(Buffer.from(nodlePublicKey));
+    const memoHash = byteToHexString(hasher.digest());
 
     // We save the mapping to firebase
     await admin.firestore().collection('bridge-accounts').doc(memoHash).set({
