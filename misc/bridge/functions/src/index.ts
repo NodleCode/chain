0af8563cbd7eb743f4ae53c38d4b9c94d7c860ed
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-05-22 10:28:39 -0700

    basic register memo function

--- misc/bridge/functions/src/index.ts
+++ misc/bridge/functions/src/index.ts
@@ -1,8 +1,21 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import Blake2s from 'blake2s-js';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const registerMemo = functions.https.onRequest(async (request, response) => {
+    const nodlePublicKey = request.body.ss58Address;
+
+    // In order to generate a deterministic ID we hash the user's address.
+    const hasher = new Blake2s(32);
+    hasher.update(new TextEncoder().encode(nodlePublicKey));
+    const memoHash = hasher.hexDigest();
+
+    // We save the mapping to firebase
+    await admin.firestore().collection('bridge-accounts').doc(memoHash).set({
+        address: nodlePublicKey,
+    }, { merge: true });
+
+    response.send({ memoHash: memoHash });
+});
