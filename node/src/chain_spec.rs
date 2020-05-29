e8b0de8526a16f08a26e79f5da7f7162055d7679
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-05-29 12:34:37 -0700

    genesis explanation of the grants module

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -106,18 +106,19 @@ pub fn testnet_genesis(
             get_account_id_from_seed::<sr25519::Public>("Charlie"),
             get_account_id_from_seed::<sr25519::Public>("Dave"),
             get_account_id_from_seed::<sr25519::Public>("Eve"),
-            get_account_id_from_seed::<sr25519::Public>("Ferdie"),
             get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
             get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
             get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
             get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
             get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-            get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
         ]
     });
 
     let vested_grants = grants.unwrap_or_else(|| {
         vec![(
+            // Ferdie has a network launch grant:
+            // 1. after 1000 blocks a cliff of 1000 NODL unlocks
+            // 2. for the next 100000 blocks a grant of 100 NODL unlocks every 1000 blocks
             get_account_id_from_seed::<sr25519::Public>("Ferdie"),
             vec![
                 (1_000, 1, 1, 1000 * NODL),      // Cliff
