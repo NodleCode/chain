050302803938c1cbf6ecdcbad1c74eb36bd6fcbb
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2020-08-18 19:52:31 -0700

    fix amendments pallet for system upgrades (#266)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -173,7 +173,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     /// Version of the runtime specification. A full-node will not attempt to use its native
     /// runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
     /// `spec_version` and `authoring_version` are the same between Wasm and native.
-    spec_version: 37,
+    spec_version: 38,
 
     /// Version of the implementation of the specification. Nodes are free to ignore this; it
     /// serves only as an indication that the code is different; as long as the other two versions
