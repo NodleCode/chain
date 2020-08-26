5ddd52083872aa089234feb543bf369c4eb8ea28
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-08-26 14:37:55 -0700

    invert tx fee split

--- runtime/src/implementations.rs
+++ runtime/src/implementations.rs
@@ -42,8 +42,8 @@ pub struct DealWithFees;
 impl OnUnbalanced<NegativeImbalance> for DealWithFees {
     fn on_unbalanceds<B>(mut fees_then_tips: impl Iterator<Item = NegativeImbalance>) {
         if let Some(fees) = fees_then_tips.next() {
-            // for fees, 80% to treasury, 20% to author
-            let mut split = fees.ration(80, 20);
+            // for fees, 20% to treasury, 80% to author
+            let mut split = fees.ration(20, 80);
             if let Some(tips) = fees_then_tips.next() {
                 // for tips, if any, 80% to treasury, 20% to author (though this can be anything)
                 tips.ration_merge_into(80, 20, &mut split);
