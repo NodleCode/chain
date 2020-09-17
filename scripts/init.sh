35e4bcc336ba36d12d5a910d656c2da451eeb16f
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-09-17 18:05:57 +0200

    freeze to last known nightly when building

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
+LAST_KNOWN_WORKING_RUST_VERSION=nightly-2020-06-01
 
 echo "*** Initializing WASM build environment"
 
-if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly
-   rustup update stable
-fi
-
-rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup default stable
+rustup uninstall nightly
+rustup toolchain install $LAST_KNOWN_WORKING_RUST_VERSION
+rustup target add wasm32-unknown-unknown --toolchain $LAST_KNOWN_WORKING_RUST_VERSION
