38e5ecaf580907b8488b778597dfe6af7a812528
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-03-23 14:12:55 +0100

    fix toolchain init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,5 +9,4 @@ if [ -z $CI_PROJECT_NAME ] ; then
    rustup update stable
 fi
 
-rustup toolchain install `cat rust-toolchain`
-rustup target add wasm32-unknown-unknown --toolchain `cat rust-toolchain`
+rustup target add wasm32-unknown-unknown --toolchain nightly
