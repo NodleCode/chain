f64978409e824159fa257c16de52da5b34022f57
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-05-10 13:57:53 +0200

    fix WASM_BINARY expect string

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -29,8 +29,8 @@ include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 pub fn wasm_binary_unwrap() -> &'static [u8] {
     WASM_BINARY.expect(
         "Development wasm binary is not available. This means the client is \
-                        built with `SKIP_WASM_BUILD` flag and it is only usable for \
-                        production chains. Please rebuild with the flag disabled.",
+        built with `SKIP_WASM_BUILD` flag and it is only usable for \
+        production chains. Please rebuild with the flag disabled.",
     )
 }
 
