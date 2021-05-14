d7f4a44e3164717d1561e544e8b8ec77c3f2e949
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-05-14 20:11:50 +0200

    remove migrations

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -63,7 +63,6 @@ pub use pallets_consensus::StakerStatus;
 
 pub mod constants;
 mod implementations;
-mod migrations;
 mod pallets_consensus;
 mod pallets_governance;
 mod pallets_nodle;
@@ -186,7 +185,6 @@ pub type Executive = frame_executive::Executive<
     frame_system::ChainContext<Runtime>,
     Runtime,
     AllModules,
-    migrations::GrantsMigration,
 >;
 
 sp_api::impl_runtime_apis! {
