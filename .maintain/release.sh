94597d31b8e6fbd8ae4b0e8b37f026fd2d6d35d8
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-06-28 11:06:58 +0200

    build before commit

--- .maintain/release.sh
+++ .maintain/release.sh
@@ -4,10 +4,10 @@ VERSION_STRING="version = \"$1\""
 
 find . -name Cargo.toml -not -path "./target/*" -exec sed -i '' -e "s/^version = .*/${VERSION_STRING}/" {} \;
 
+cargo build --release
+
 git add .
 git commit -m "bump version for release"
 git push
-
-cargo build --release
 
 gh release create $1 --title $1 --target master 'target/release/wbuild/nodle-chain-runtime/nodle_chain_runtime.compact.wasm#Runtime Blob'
