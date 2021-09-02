c293e614b089a09e7275ecd6bcc23c9c58667c80
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-09-02 15:55:35 +0200

    fine tune release script to use srtool runtime

--- .maintain/release.sh
+++ .maintain/release.sh
@@ -4,11 +4,13 @@ VERSION_STRING="version = \"$1\""
 
 find . -name Cargo.toml -not -path "./target/*" -exec sed -i '' -e "s/^version = .*/${VERSION_STRING}/" {} \;
 
-cargo build --release
-
 git pull
 git add .
 git commit -m "bump version for release"
 git push
 
-gh release create $1 --title $1 --target master 'target/release/wbuild/nodle-chain-runtime/nodle_chain_runtime.compact.wasm#Runtime Blob'
+echo -n "Please wait for srtool github action to complete and enter the file name: "
+read runtime_file
+
+gh run download -n $runtime_file -n nodle-chain-srtool-digest.json
+gh release create $1 --title $1 --target master 'nodle_chain_runtime.compact.wasm' 'nodle-chain-srtool-digest.json'
