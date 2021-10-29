b6314ec9a59b5aab7b1e02dca9a2997afe5dbbee
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-10-29 15:20:12 +0100

    create minimum staking runtime

---
+++ runtimes/staking/src/lib.rs
@@ -0,0 +1,382 @@
+/*
+ * This file is part of the Nodle Chain distributed at https://github.com/NodleCode/chain
+ * Copyright (C) 2020  Nodle International
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#![cfg_attr(not(feature = "std"), no_std)]
+// `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
+#![recursion_limit = "256"]
+
+// Make the WASM binary available.
+#[cfg(feature = "std")]
+include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+
+/// Wasm binary unwrapped. If built with `SKIP_WASM_BUILD`, the function panics.
+#[cfg(feature = "std")]
+pub fn wasm_binary_unwrap() -> &'static [u8] {
+    WASM_BINARY.expect(
+        "Development wasm binary is not available. This means the client is \
+        built with `SKIP_WASM_BUILD` flag and it is only usable for \
+        production chains. Please rebuild with the flag disabled.",
+    )
+}
+
+use frame_support::{
+    construct_runtime,
+    traits::{KeyOwnerProofSystem, Randomness},
+};
+use nodle_chain_primitives::{AccountId, Balance, BlockNumber, Index, Signature};
+use pallet_grandpa::{
+    fg_primitives, AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList,
+};
+use pallet_session::historical as pallet_session_historical;
+use pallet_transaction_payment::{FeeDetails, RuntimeDispatchInfo};
+use sp_authority_discovery::AuthorityId as AuthorityDiscoveryId;
+use sp_core::OpaqueMetadata;
+#[cfg(any(feature = "std", test))]
+pub use sp_runtime::BuildStorage;
+use sp_runtime::{
+    generic,
+    traits::{BlakeTwo256, Block as BlockT, NumberFor},
+    transaction_validity::{TransactionSource, TransactionValidity},
+    ApplyExtrinsicResult,
+};
+use sp_std::prelude::*;
+use sp_version::RuntimeVersion;
+
+pub mod constants;
+mod pallets_consensus;
+mod pallets_governance;
+mod pallets_system;
+mod pallets_util;
+mod version;
+
+pub use pallets_consensus::EpochDuration;
+pub use pallets_consensus::SessionKeys;
+#[cfg(feature = "std")]
+pub use version::native_version;
+pub use version::VERSION;
+
+construct_runtime! {
+    pub enum Runtime where
+        Block = Block,
+        NodeBlock = nodle_chain_primitives::Block,
+        UncheckedExtrinsic = UncheckedExtrinsic
+    {
+        // System
+        System: frame_system::{Module, Call, Storage, Config, Event<T>} = 0,
+        Timestamp: pallet_timestamp::{Module, Call, Storage, Inherent} = 1,
+        Balances: pallet_balances::{Module, Call, Storage, Config<T>, Event<T>} = 2,
+        TransactionPayment: pallet_transaction_payment::{Module, Storage} = 3,
+        RandomnessCollectiveFlip: pallet_randomness_collective_flip::{Module, Call, Storage} = 4,
+
+        // Consensus
+        Babe: pallet_babe::{Module, Call, Storage, Config, Inherent, ValidateUnsigned} = 5,
+        Grandpa: pallet_grandpa::{Module, Call, Storage, Config, Event, ValidateUnsigned} = 6,
+        Authorship: pallet_authorship::{Module, Call, Storage, Inherent} = 7,
+        ImOnline: pallet_im_online::{Module, Call, Storage, Event<T>, ValidateUnsigned, Config<T>} = 8,
+        Offences: pallet_offences::{Module, Call, Storage, Event} = 9,
+        NodleStaking: pallet_nodle_staking::{Module, Call, Storage, Event<T>, Config<T>} = 11,
+        Session: pallet_session::{Module, Call, Storage, Event, Config<T>} = 12,
+        Historical: pallet_session_historical::{Module} = 13,
+        AuthorityDiscovery: pallet_authority_discovery::{Module, Call, Config} = 14,
+
+        // Governance
+        RootCommittee: pallet_collective::{Module, Call, Storage, Origin<T>, Event<T>, Config<T>} = 19,
+        RootMembership: pallet_membership::{Module, Call, Storage, Event<T>, Config<T>} = 20,
+        Scheduler: pallet_scheduler::{Module, Call, Storage, Event<T>} = 21,
+        Mandate: pallet_mandate::{Module, Call, Event} = 23,
+
+        // Neat things
+        Utility: pallet_utility::{Module, Call, Event} = 28,
+        Multisig: pallet_multisig::{Module, Call, Storage, Event<T>} = 29,
+    }
+}
+
+/// The address format for describing accounts.
+pub type Address = sp_runtime::MultiAddress<AccountId, ()>;
+/// Block header type as expected by this runtime.
+pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
+/// Block type as expected by this runtime.
+pub type Block = generic::Block<Header, UncheckedExtrinsic>;
+/// A Block signed with a Justification
+pub type SignedBlock = generic::SignedBlock<Block>;
+/// BlockId type as expected by this runtime.
+pub type BlockId = generic::BlockId<Block>;
+/// The SignedExtension to the basic transaction logic.
+pub type SignedExtra = (
+    frame_system::CheckSpecVersion<Runtime>,
+    frame_system::CheckTxVersion<Runtime>,
+    frame_system::CheckGenesis<Runtime>,
+    frame_system::CheckEra<Runtime>,
+    frame_system::CheckNonce<Runtime>,
+    frame_system::CheckWeight<Runtime>,
+    pallet_transaction_payment::ChargeTransactionPayment<Runtime>,
+);
+/// Unchecked extrinsic type as expected by this runtime.
+pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signature, SignedExtra>;
+/// The payload being signed in transactions.
+pub type SignedPayload = generic::SignedPayload<Call, SignedExtra>;
+/// Extrinsic type that has already been checked.
+pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, Call, SignedExtra>;
+
+/// Executive: handles dispatch to the various modules.
+pub type Executive = frame_executive::Executive<
+    Runtime,
+    Block,
+    frame_system::ChainContext<Runtime>,
+    Runtime,
+    AllModules,
+>;
+
+sp_api::impl_runtime_apis! {
+    impl sp_api::Core<Block> for Runtime {
+        fn version() -> RuntimeVersion {
+            version::VERSION
+        }
+
+        fn execute_block(block: Block) {
+            Executive::execute_block(block)
+        }
+
+        fn initialize_block(header: &<Block as BlockT>::Header) {
+            Executive::initialize_block(header)
+        }
+    }
+
+    impl sp_api::Metadata<Block> for Runtime {
+        fn metadata() -> OpaqueMetadata {
+            Runtime::metadata().into()
+        }
+    }
+
+    impl sp_block_builder::BlockBuilder<Block> for Runtime {
+        fn apply_extrinsic(extrinsic: <Block as BlockT>::Extrinsic) -> ApplyExtrinsicResult {
+            Executive::apply_extrinsic(extrinsic)
+        }
+
+        fn finalize_block() -> <Block as BlockT>::Header {
+            Executive::finalize_block()
+        }
+
+        fn inherent_extrinsics(data: sp_inherents::InherentData) -> Vec<<Block as BlockT>::Extrinsic> {
+            data.create_extrinsics()
+        }
+
+        fn check_inherents(
+            block: Block,
+            data: sp_inherents::InherentData,
+        ) -> sp_inherents::CheckInherentsResult {
+            data.check_extrinsics(&block)
+        }
+
+        fn random_seed() -> <Block as BlockT>::Hash {
+            RandomnessCollectiveFlip::random_seed()
+        }
+    }
+
+    impl sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block> for Runtime {
+        fn validate_transaction(source: TransactionSource, tx: <Block as BlockT>::Extrinsic) -> TransactionValidity {
+            Executive::validate_transaction(source, tx)
+        }
+    }
+
+    impl sp_offchain::OffchainWorkerApi<Block> for Runtime {
+        fn offchain_worker(header: &<Block as BlockT>::Header) {
+            Executive::offchain_worker(header)
+        }
+    }
+
+    impl fg_primitives::GrandpaApi<Block> for Runtime {
+        fn grandpa_authorities() -> GrandpaAuthorityList {
+            Grandpa::grandpa_authorities()
+        }
+
+        fn submit_report_equivocation_unsigned_extrinsic(
+            equivocation_proof: fg_primitives::EquivocationProof<
+                <Block as BlockT>::Hash,
+                NumberFor<Block>,
+            >,
+            key_owner_proof: fg_primitives::OpaqueKeyOwnershipProof,
+        ) -> Option<()> {
+            let key_owner_proof = key_owner_proof.decode()?;
+
+            Grandpa::submit_unsigned_equivocation_report(
+                equivocation_proof,
+                key_owner_proof,
+            )
+        }
+
+        fn generate_key_ownership_proof(
+            _set_id: fg_primitives::SetId,
+            authority_id: GrandpaId,
+        ) -> Option<fg_primitives::OpaqueKeyOwnershipProof> {
+            use parity_scale_codec::Encode;
+
+            Historical::prove((fg_primitives::KEY_TYPE, authority_id))
+                .map(|p| p.encode())
+                .map(fg_primitives::OpaqueKeyOwnershipProof::new)
+        }
+    }
+
+    impl sp_consensus_babe::BabeApi<Block> for Runtime {
+        fn configuration() -> sp_consensus_babe::BabeGenesisConfiguration {
+            // The choice of `c` parameter (where `1 - c` represents the
+            // probability of a slot being empty), is done in accordance to the
+            // slot duration and expected target block time, for safely
+            // resisting network delays of maximum two seconds.
+            // <https://research.web3.foundation/en/latest/polkadot/BABE/Babe/#6-practical-results>
+            sp_consensus_babe::BabeGenesisConfiguration {
+                slot_duration: Babe::slot_duration(),
+                epoch_length: EpochDuration::get(),
+                c: crate::constants::PRIMARY_PROBABILITY,
+                genesis_authorities: Babe::authorities(),
+                randomness: Babe::randomness(),
+                allowed_slots: sp_consensus_babe::AllowedSlots::PrimaryAndSecondaryPlainSlots,
+            }
+        }
+
+        fn current_epoch_start() -> sp_consensus_babe::Slot {
+            Babe::current_epoch_start()
+        }
+
+        fn current_epoch() -> sp_consensus_babe::Epoch {
+            Babe::current_epoch()
+        }
+
+        fn next_epoch() -> sp_consensus_babe::Epoch {
+            Babe::next_epoch()
+        }
+
+        fn generate_key_ownership_proof(
+            _slot_number: sp_consensus_babe::Slot,
+            authority_id: sp_consensus_babe::AuthorityId,
+        ) -> Option<sp_consensus_babe::OpaqueKeyOwnershipProof> {
+            use parity_scale_codec::Encode;
+            Historical::prove((sp_consensus_babe::KEY_TYPE, authority_id))
+                .map(|p| p.encode())
+                .map(sp_consensus_babe::OpaqueKeyOwnershipProof::new)
+        }
+
+        fn submit_report_equivocation_unsigned_extrinsic(
+            equivocation_proof: sp_consensus_babe::EquivocationProof<<Block as BlockT>::Header>,
+            key_owner_proof: sp_consensus_babe::OpaqueKeyOwnershipProof,
+        ) -> Option<()> {
+            let key_owner_proof = key_owner_proof.decode()?;
+
+            Babe::submit_unsigned_equivocation_report(
+                equivocation_proof,
+                key_owner_proof,
+            )
+        }
+    }
+
+    impl sp_authority_discovery::AuthorityDiscoveryApi<Block> for Runtime {
+        fn authorities() -> Vec<AuthorityDiscoveryId> {
+            AuthorityDiscovery::authorities()
+        }
+    }
+
+    impl sp_session::SessionKeys<Block> for Runtime {
+        fn generate_session_keys(seed: Option<Vec<u8>>) -> Vec<u8> {
+            SessionKeys::generate(seed)
+        }
+
+        fn decode_session_keys(
+            encoded: Vec<u8>,
+        ) -> Option<Vec<(Vec<u8>, sp_core::crypto::KeyTypeId)>> {
+            SessionKeys::decode_into_raw_public_keys(&encoded)
+        }
+    }
+
+    impl frame_system_rpc_runtime_api::AccountNonceApi<Block, AccountId, Index> for Runtime {
+        fn account_nonce(account: AccountId) -> Index {
+            System::account_nonce(account)
+        }
+    }
+
+    impl pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<
+        Block,
+        Balance,
+    > for Runtime {
+        fn query_info(uxt: <Block as BlockT>::Extrinsic, len: u32) -> RuntimeDispatchInfo<Balance> {
+            TransactionPayment::query_info(uxt, len)
+        }
+
+        fn query_fee_details(uxt: <Block as BlockT>::Extrinsic, len: u32) -> FeeDetails<Balance> {
+            TransactionPayment::query_fee_details(uxt, len)
+        }
+    }
+
+    #[cfg(feature = "runtime-benchmarks")]
+    impl frame_benchmarking::Benchmark<Block> for Runtime {
+        fn dispatch_benchmark(
+            config: frame_benchmarking::BenchmarkConfig,
+        ) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, sp_runtime::RuntimeString> {
+            // We did not include the offences and sessions benchmarks as they are parity
+            // specific and were causing some issues at compile time as they depend on the
+            // presence of the staking and elections pallets.
+
+            use frame_benchmarking::{Benchmarking, BenchmarkBatch, TrackedStorageKey, add_benchmark};
+
+            use frame_system_benchmarking::Module as SystemBench;
+            //use pallet_offences_benchmarking::Module as OffencesBench;
+            //use pallet_session_benchmarking::Module as SessionBench;
+
+            impl frame_system_benchmarking::Config for Runtime {}
+            //impl pallet_offences_benchmarking::Config for Runtime{}
+            //impl pallet_session_benchmarking::Config for Runtime {}
+
+            let whitelist: Vec<TrackedStorageKey> = vec![];
+            let mut batches = Vec::<BenchmarkBatch>::new();
+            let params = (&config, &whitelist);
+
+            add_benchmark!(params, batches, frame_system, SystemBench::<Runtime>);
+            add_benchmark!(params, batches, pallet_babe, Babe);
+            add_benchmark!(params, batches, pallet_balances, Balances);
+            add_benchmark!(params, batches, pallet_collective, RootCommittee);
+            add_benchmark!(params, batches, pallet_grandpa, Grandpa);
+            add_benchmark!(params, batches, pallet_nodle_staking, NodleStaking);
+            add_benchmark!(params, batches, pallet_im_online, ImOnline);
+            add_benchmark!(params, batches, pallet_multisig, Multisig);
+            //add_benchmark!(params, batches, pallet_offences, OffencesBench::<Runtime>);
+            //add_benchmark!(params, batches, pallet_session, SessionBench::<Runtime>);
+            add_benchmark!(params, batches, pallet_scheduler, Scheduler);
+            add_benchmark!(params, batches, pallet_timestamp, Timestamp);
+            add_benchmark!(params, batches, pallet_utility, Utility);
+
+            if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
+            Ok(batches)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use frame_system::offchain::CreateSignedTransaction;
+
+    #[test]
+    fn validate_transaction_submitter_bounds() {
+        fn is_submit_signed_transaction<T>()
+        where
+            T: CreateSignedTransaction<Call>,
+        {
+        }
+
+        is_submit_signed_transaction::<Runtime>();
+    }
+}
---
+++ runtimes/staking/src/pallets_consensus.rs
@@ -0,0 +1,210 @@
+/*
+ * This file is part of the Nodle Chain distributed at https://github.com/NodleCode/chain
+ * Copyright (C) 2020  Nodle International
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use crate::{
+    constants, pallets_governance::RootCollective, AuthorityDiscovery, Babe, Balances, Call, Event,
+    Grandpa, Historical, ImOnline, NodleStaking, Offences, Runtime, Session,
+};
+use frame_support::{
+    parameter_types,
+    traits::{KeyOwnerProofSystem, LockIdentifier},
+    weights::Weight,
+};
+use nodle_chain_primitives::{AccountId, Balance, BlockNumber, Moment};
+use pallet_grandpa::AuthorityId as GrandpaId;
+use pallet_im_online::sr25519::AuthorityId as ImOnlineId;
+use sp_core::{
+    crypto::KeyTypeId,
+    u32_trait::{_1, _2},
+};
+use sp_runtime::{
+    impl_opaque_keys, traits::OpaqueKeys, transaction_validity::TransactionPriority, ModuleId,
+    Perbill,
+};
+use sp_std::prelude::*;
+
+impl_opaque_keys! {
+    pub struct SessionKeys {
+        pub babe: Babe,
+        pub grandpa: Grandpa,
+        pub im_online: ImOnline,
+        pub authority_discovery: AuthorityDiscovery,
+    }
+}
+
+// Normally used with the staking pallet
+parameter_types! {
+    // 28 Days for unbonding
+    pub const BondingDuration: sp_staking::SessionIndex = 28 * 6;
+}
+
+parameter_types! {
+    pub const EpochDuration: u64 = constants::EPOCH_DURATION_IN_SLOTS;
+    pub const ExpectedBlockTime: Moment = constants::MILLISECS_PER_BLOCK;
+    pub const ReportLongevity: u64 =
+        BondingDuration::get() as u64 * EpochDuration::get();
+}
+
+impl pallet_babe::Config for Runtime {
+    type EpochDuration = EpochDuration;
+    type ExpectedBlockTime = ExpectedBlockTime;
+    type EpochChangeTrigger = pallet_babe::ExternalTrigger;
+    type KeyOwnerProofSystem = Historical;
+    type KeyOwnerProof = <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(
+        KeyTypeId,
+        pallet_babe::AuthorityId,
+    )>>::Proof;
+    type KeyOwnerIdentification = <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(
+        KeyTypeId,
+        pallet_babe::AuthorityId,
+    )>>::IdentificationTuple;
+    type HandleEquivocation =
+        pallet_babe::EquivocationHandler<Self::KeyOwnerIdentification, Offences, ReportLongevity>;
+    type WeightInfo = ();
+}
+
+impl pallet_grandpa::Config for Runtime {
+    type Event = Event;
+    type Call = Call;
+    type KeyOwnerProofSystem = Historical;
+    type KeyOwnerProof =
+        <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(KeyTypeId, GrandpaId)>>::Proof;
+    type KeyOwnerIdentification = <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(
+        KeyTypeId,
+        GrandpaId,
+    )>>::IdentificationTuple;
+    type HandleEquivocation = pallet_grandpa::EquivocationHandler<
+        Self::KeyOwnerIdentification,
+        Offences,
+        ReportLongevity,
+    >;
+    type WeightInfo = ();
+}
+
+impl pallet_authority_discovery::Config for Runtime {}
+
+parameter_types! {
+    pub const UncleGenerations: u32 = 5;
+}
+
+impl pallet_authorship::Config for Runtime {
+    type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Babe>;
+    type UncleGenerations = UncleGenerations;
+    type FilterUncle = ();
+    type EventHandler = (NodleStaking, ImOnline);
+}
+
+parameter_types! {
+    pub const SessionDuration: BlockNumber = constants::EPOCH_DURATION_IN_SLOTS as _;
+    pub const ImOnlineUnsignedPriority: TransactionPriority = TransactionPriority::max_value();
+}
+
+impl pallet_im_online::Config for Runtime {
+    type AuthorityId = ImOnlineId;
+    type Event = Event;
+    type ValidatorSet = Historical;
+    type SessionDuration = SessionDuration;
+    type ReportUnresponsiveness = Offences;
+    type UnsignedPriority = ImOnlineUnsignedPriority;
+    type WeightInfo = pallet_im_online::weights::SubstrateWeight<Runtime>;
+}
+
+parameter_types! {
+    // When this percentage is reached the module will force a new era, we never
+    // call `session.disable()` so this should never be used.
+    pub const DisabledValidatorsThreshold: Perbill = Perbill::from_percent(17);
+}
+
+impl pallet_session::Config for Runtime {
+    type SessionManager = NodleStaking;
+    type SessionHandler = <SessionKeys as OpaqueKeys>::KeyTypeIdProviders;
+    type ShouldEndSession = Babe;
+    type Event = Event;
+    type Keys = SessionKeys;
+    type ValidatorId = AccountId;
+    type ValidatorIdOf = pallet_nodle_staking::StashOf<Runtime>;
+    type DisabledValidatorsThreshold = DisabledValidatorsThreshold;
+    type NextSessionRotation = Babe;
+    type WeightInfo = pallet_session::weights::SubstrateWeight<Runtime>;
+}
+
+impl pallet_session::historical::Config for Runtime {
+    type FullIdentification = pallet_nodle_staking::ValidatorSnapshot<AccountId, Balance>;
+    type FullIdentificationOf = pallet_nodle_staking::ValidatorSnapshotOf<Runtime>;
+}
+
+// TODO::Have to fine tune parameters for practical use-case
+parameter_types! {
+    // 27 Days
+    pub const SlashDeferDuration: sp_staking::SessionIndex = 27 * 6;
+    pub const MinSelectedValidators: u32 = 5;
+    pub const MaxNominatorsPerValidator: u32 = 25;
+    pub const MaxValidatorPerNominator: u32 = 25;
+    pub const DefaultValidatorFee: Perbill = Perbill::from_percent(20);
+    pub const DefaultSlashRewardProportion: Perbill = Perbill::from_percent(10);
+    pub const DefaultSlashRewardFraction: Perbill = Perbill::from_percent(50);
+    pub const DefaultStakingMaxValidators: u32 = 50;
+    pub const DefaultStakingMinStakeSessionSelection: Balance = 10 * constants::MILLICENTS;
+    pub const DefaultStakingMinValidatorBond: Balance = 10 * constants::MILLICENTS;
+    pub const DefaultStakingMinNominatorTotalBond: Balance = 10 * constants::MILLICENTS;
+    pub const DefaultStakingMinNominationChillThreshold: Balance = 3 * constants::MILLICENTS;
+    pub const MaxChunkUnlock: usize = 32;
+    pub const StakingPalletId: ModuleId = ModuleId(*b"mockstak");
+    pub const StakingLockId: LockIdentifier = *b"staking ";
+}
+impl pallet_nodle_staking::Config for Runtime {
+    type Event = Event;
+    type Currency = Balances;
+    type BondedDuration = BondingDuration;
+    type MinSelectedValidators = MinSelectedValidators;
+    type MaxNominatorsPerValidator = MaxNominatorsPerValidator;
+    type MaxValidatorPerNominator = MaxValidatorPerNominator;
+    type DefaultValidatorFee = DefaultValidatorFee;
+    type DefaultSlashRewardProportion = DefaultSlashRewardProportion;
+    type DefaultSlashRewardFraction = DefaultSlashRewardFraction;
+    type DefaultStakingMaxValidators = DefaultStakingMaxValidators;
+    type DefaultStakingMinValidatorBond = DefaultStakingMinValidatorBond;
+    type DefaultStakingMinStakeSessionSelection = DefaultStakingMinStakeSessionSelection;
+    type DefaultStakingMinNominatorTotalBond = DefaultStakingMinNominatorTotalBond;
+    type DefaultStakingMinNominationChillThreshold = DefaultStakingMinNominationChillThreshold;
+    type RewardRemainder = ();
+    type MaxChunkUnlock = MaxChunkUnlock;
+    type PalletId = StakingPalletId;
+    type StakingLockId = StakingLockId;
+    type Slash = ();
+    type SlashDeferDuration = SlashDeferDuration;
+    type SessionInterface = Self;
+    type ValidatorRegistration = Session;
+    type CancelOrigin =
+        pallet_collective::EnsureProportionMoreThan<_1, _2, AccountId, RootCollective>;
+    type WeightInfo = pallet_nodle_staking::weights::SubstrateWeight<Runtime>;
+}
+
+parameter_types! {
+    pub OffencesWeightSoftLimit: Weight = Perbill::from_percent(60) *
+        constants::RuntimeBlockWeights::get().max_block;
+}
+
+impl pallet_offences::Config for Runtime {
+    type Event = Event;
+    type IdentificationTuple = pallet_session::historical::IdentificationTuple<Self>;
+    type OnOffenceHandler = NodleStaking;
+    type WeightSoftLimit = OffencesWeightSoftLimit;
+}
