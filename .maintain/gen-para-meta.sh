94a7aeffdd03f3ac16dd3dc7bdf7087065a3621d
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-11-25 22:50:07 +0100

    include parachain-id option when generating head

--- .maintain/gen-para-meta.sh
+++ .maintain/gen-para-meta.sh
@@ -2,5 +2,5 @@ CHAIN="eden-dev"
 
 cargo build
 
-./target/debug/nodle-chain export-genesis-state --chain $CHAIN > .maintain/head.hex
+./target/debug/nodle-chain export-genesis-state --chain $CHAIN --parachain-id 2000 > .maintain/head.hex
 ./target/debug/nodle-chain export-genesis-wasm --chain $CHAIN > .maintain/wasm.hex
