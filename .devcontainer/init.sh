f4bc7185bf6c4dcd6cc9025d9a38e7350b155ed2
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-11-25 10:54:29 +0100

    fix dev build
    (cherry picked from commit a393b1f1915331fae856bd1bb9b720a4712c77ff)

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -7,18 +7,19 @@ export PATH=$PATH:$HOME/.cargo/bin
 
 # export our artefacts and cache dependencies in the process
 echo "Building artefacts and preloading dependencies..."
-cargo run -- export-genesis-head --chain $CHAIN > /tmp/head.hex 2>> /tmp/init.log
-cargo run -- export-genesis-wasm --chain $CHAIN > /tmp/wasm.hex 2>> /tmp/init.log
+./scripts/init.sh
+cargo run -- export-genesis-state --chain $CHAIN > /tmp/head.hex
+cargo run -- export-genesis-wasm --chain $CHAIN > /tmp/wasm.hex
 
 # wait for relay chain node to be available
 echo "Waiting for relay chain node..."
-while ! nc -z node-polkadot 9944; do   
-  sleep 0.1 # wait for 1/10 of the second before check again
+while ! nc -z node-polkadot-alice 9944; do   
+  sleep 1
 done
 
-# register the parachain on the relay chain
-echo "Registering parachain..."
-polkadot-js-api --ws ws://node-polkadot:9944 --seed "//Alice" tx
+# register and onboard the parachain on the relay chain
+echo "Registering parachain (should take about 2 minutes)..."
+polkadot-js-api --ws ws://node-polkadot-alice:9944 --seed "//Alice" --sudo tx.parasSudoWrapper.sudoScheduleParaInitialize 2000 '["`cat /tmp/head.hex`", "`cat /tmp/wasm.hex`", true]'
 
 # keep running for vscode
 echo "Ready for battle"
