576d85a30c9093d9d3095937c810667abb54ca20
ETeissonniere <ETeissonniere@users.noreply.github.com>
2021-12-06 19:50:05 +0000

    deploy: 8fe0c22d313ba8bf6a7e5d9a386c4ce2858f6a12

--- implementors/sc_chain_spec/extension/trait.Fork.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["nodle_chain"] = [{"text":"impl <a class=\"trait\" href=\"sc_chain_spec/extension/trait.Fork.html\" title=\"trait sc_chain_spec::extension::Fork\">Fork</a> for <a class=\"struct\" href=\"nodle_chain/chain_spec/cs_eden/struct.ExtensionsFork.html\" title=\"struct nodle_chain::chain_spec::cs_eden::ExtensionsFork\">ExtensionsFork</a>","synthetic":false,"types":["nodle_chain::chain_spec::cs_eden::ExtensionsFork"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
