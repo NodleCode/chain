b541f3f36b8821be9d32ed49703fd874017b9772
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-12-07 11:03:09 -0800

    generalize session key injection to aura and collator

--- .maintain/docker/load_keys.sh
+++ .maintain/docker/load_keys.sh
@@ -18,7 +18,8 @@ while ! nc -z localhost 9933; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
-insert_key "babe" $BABE_SK $BABE_PK
-insert_key "gran" $GRANDPA_SK $GRANDPA_PK
-insert_key "imon" $IM_ONLINE_SK $IM_ONLINE_PK
-insert_key "auth" $AUTHORITY_DISCOVERY_SK $AUTHORITY_DISCOVERY_PK
+insert_key "aura" $AURA_SK $AURA_PK || echo "no aura keys"
+insert_key "babe" $BABE_SK $BABE_PK || echo "no babe keys"
+insert_key "gran" $GRANDPA_SK $GRANDPA_PK || echo "no grandpa keys"
+insert_key "imon" $IM_ONLINE_SK $IM_ONLINE_PK || echo "no im_online keys"
+insert_key "auth" $AUTHORITY_DISCOVERY_SK $AUTHORITY_DISCOVERY_PK || echo "no authority_discovery keys"
