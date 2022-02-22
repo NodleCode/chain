101c0bc7ee37e30d0542e04dfadbeae911f87673
aliXsed <aliXsed@users.noreply.github.com>
2022-02-22 01:20:37 +0000

    deploy: df9831f8a3f71d8162c1d269c6889d7b3addbc87

---
+++ implementors/pallet_wnodl/weights/trait.WeightInfo.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pallet_wnodl"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
