5cab44a48af2455fd1986c00909ba9496aa6e41e
Eliott Teissonniere <git.eliott@teissonniere.org>
2022-03-15 11:47:30 +0000

    default to eden chain spec

--- nodes/parachain/src/command.rs
+++ nodes/parachain/src/command.rs
@@ -47,7 +47,7 @@ fn load_spec(id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, St
     Ok(match id {
         "eden-dev" => Box::new(chain_spec::development_config(DEFAULT_PARA_ID.into())),
         "eden-local" => Box::new(chain_spec::local_testnet_config(DEFAULT_PARA_ID.into())),
-        "eden" => Box::new(chain_spec::main_config()),
+        "" | "eden" => Box::new(chain_spec::main_config()),
         path => Box::new(chain_spec::ChainSpec::from_json_file(
             std::path::PathBuf::from(path),
         )?),
