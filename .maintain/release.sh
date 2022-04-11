0cf4c36c259c1b3870295114d2a61228b7869eab
Eliott Teissonniere <git.eliott@teissonniere.org>
2022-04-11 12:01:40 +0000

    make release script cross platform

--- .maintain/release.sh
+++ .maintain/release.sh
@@ -1,9 +1,19 @@
 # this script can be used to prepare a new release for this repo
 
+SED_OPT=""
+case "$(uname -s)" in
+   Darwin)
+        SED_OPT="-i ''"
+     ;;
+    Linux)
+        SED_OPT="-i"
+     ;;
+esac
+
 git pull
 
 VERSION_STRING="version = \"$1\""
-find . -name Cargo.toml -not -path "./target/*" -exec sed -i '' -e "s/^version = .*/${VERSION_STRING}/" {} \;
+find . -name Cargo.toml -not -path "./target/*" -exec sed ${SED_OPT} -e "s/^version = .*/${VERSION_STRING}/" {} \;
 cargo test --all --all-features
 
 echo "Please verify script execution and press enter or ctrl-c"
