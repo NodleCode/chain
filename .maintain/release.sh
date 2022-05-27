aefc652ded3848d5c439d30d987cd89688312282
Eliott Teissonniere <git.eliott@teissonniere.org>
2022-05-27 11:59:16 +0000

    modify script to be PR friendly

--- .maintain/release.sh
+++ .maintain/release.sh
@@ -14,17 +14,3 @@ git pull
 
 VERSION_STRING="version = \"$1\""
 find . -name Cargo.toml -not -path "./target/*" -exec sed ${SED_OPT} -e "s/^version = .*/${VERSION_STRING}/" {} \;
-cargo test --all --all-features
-
-echo "Please verify script execution and press enter or ctrl-c"
-read sanity_check
-
-git add .
-git commit -m "bump version for release"
-git push
-
-echo "Please wait for srtool github action to complete and enter the file name: "
-read runtime_file
-
-gh run download -n $runtime_file
-gh release create $1 --title $1 --target master "$runtime_file/runtimes/eden/target/srtool/release/wbuild/runtime-eden/runtime_eden.compact.wasm" "$runtime_file/runtimes/eden/target/srtool/release/wbuild/runtime-eden/runtime_eden.compact.compressed.wasm" "$runtime_file/eden-srtool-digest.json"
