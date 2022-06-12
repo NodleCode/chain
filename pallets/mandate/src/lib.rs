5584bf6fcdfb2a56eb4b45092bdff8c43b66a70d
R.Rajeshkumar <rajesh@nodle.com>
2022-06-12 22:34:31 +0530

    Remove without_storage_info for the mandate pallet
    Signed-off-by: R.Rajeshkumar <rajesh@nodle.com>

--- pallets/mandate/src/lib.rs
+++ pallets/mandate/src/lib.rs
@@ -47,7 +47,6 @@ pub mod pallet {
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
-	#[pallet::without_storage_info]
 	pub struct Pallet<T>(PhantomData<T>);
 
 	#[pallet::hooks]
