06439aca2cd063fd7b4e93221d45805e35333955
rajesh-nodle <82595429+rajesh-nodle@users.noreply.github.com>
2022-07-13 11:00:36 +0530

    Update pallets/grants/src/benchmarking.rs
    Co-authored-by: Fredrik Simonsson <simson@thesimson.net>

--- pallets/grants/src/benchmarking.rs
+++ pallets/grants/src/benchmarking.rs
@@ -68,7 +68,7 @@ benchmarks! {
 		let config = create_shared_config::<T>(1);
 
 		// Add some existing schedules according to b
-		for x in 0 .. T::MaxSchedule::get().saturating_sub(1) {
+		for _x in 1 .. T::MaxSchedule::get() {
 			Pallet::<T>::do_add_vesting_schedule(&config.granter, &config.grantee, config.schedule.clone())?;
 		}
 	}:  _(RawOrigin::Signed(config.granter.clone()), config.grantee_lookup.clone(), config.schedule.clone())
