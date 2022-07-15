dce8ccc8515dc9300259123b3f71c7c6562d43c5
Alex Sedighi <alex@nodle.co>
2022-07-15 15:39:03 +1200

    chore: update srtool.sh
    Use the latest release of paritytech/srtool

--- .maintain/srtool.sh
+++ .maintain/srtool.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -e PACKAGE=runtime-eden -e RUNTIME_DIR="runtimes/eden" -v `pwd`:/build paritytech/srtool:1.56.1
+docker run --rm -it -e PACKAGE=runtime-eden -e RUNTIME_DIR="runtimes/eden" -v `pwd`:/build paritytech/srtool:1.62.0
