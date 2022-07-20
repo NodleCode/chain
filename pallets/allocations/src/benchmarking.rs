8d4e87ea0c40fdf3562f2b57b1ed9d073f1f884c
rajesh-nodle <82595429+rajesh-nodle@users.noreply.github.com>
2022-07-20 14:34:31 +0530

    remove tarpaulin ignore

--- pallets/allocations/src/benchmarking.rs
+++ pallets/allocations/src/benchmarking.rs
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#![cfg_attr(tarpaulin, ignore)]
 #![cfg(feature = "runtime-benchmarks")]
 
 //! Amendments pallet benchmarks
