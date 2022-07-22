ac6890d733841dbaa4e50ae763e1dc39c5a19d86
R.Rajeshkumar <rajesh@nodle.com>
2022-07-22 13:44:28 +0530

    runtime migration updates
    Signed-off-by: R.Rajeshkumar <rajesh@nodle.com>

--- pallets/poa/src/migrations.rs
+++ pallets/poa/src/migrations.rs
@@ -19,113 +19,104 @@
 pub mod v1 {
 	use crate::{Config, Releases, StorageVersion};
 	use frame_support::{
-		pallet_prelude::PhantomData,
-		storage::migration::{have_storage_value, remove_storage_prefix},
-		traits::{Get, OnRuntimeUpgrade},
+		storage::migration::{remove_storage_prefix},
+		traits::{Get},
 		weights::Weight,
 	};
 
-	pub struct MigrateToBoundedValidators<T>(PhantomData<T>);
-	impl<T: Config> OnRuntimeUpgrade for MigrateToBoundedValidators<T> {
-		fn on_runtime_upgrade() -> Weight {
-			log::info!(
-				"on_runtime_upgrade[{:#?}]=> Running migration with current storage version {:?} / onchain {:?}",
-				line!(),
-				crate::Releases::V2_0_21,
-				<StorageVersion<T>>::get(),
-			);
+	pub fn on_runtime_upgrade<T: Config>() -> Weight {
+		log::info!(
+			"on_runtime_upgrade[{:#?}]=> Running migration with current storage version {:?} / onchain {:?}",
+			line!(),
+			crate::Releases::V1,
+			<StorageVersion<T>>::get(),
+		);
 
-			if <StorageVersion<T>>::get() == Releases::V0_0_0Legacy {
-				let pallet_prefix: &[u8] = b"Poa";
-				let storage_item_prefix: &[u8] = b"Validators";
+		if <StorageVersion<T>>::get() == Releases::V0 {
+			let pallet_prefix: &[u8] = b"Poa";
+			let storage_item_prefix: &[u8] = b"Validators";
 
-				if have_storage_value(pallet_prefix, storage_item_prefix, &[]) {
-					remove_storage_prefix(pallet_prefix, storage_item_prefix, &[]);
+			remove_storage_prefix(pallet_prefix, storage_item_prefix, &[]);
 
-					<StorageVersion<T>>::put(crate::Releases::V2_0_21);
-
-					log::info!(
-						"on_runtime_upgrade[{:#?}]=> Removed Validators, Migrated to storage version {:?}",
-						line!(),
-						<StorageVersion<T>>::get()
-					);
-				} else {
-					panic!("on_runtime_upgrade[{:#?}]=> Validators doesn't exist", line!());
-				}
-
-				T::DbWeight::get().reads_writes(1, 1)
-			} else {
-				log::info!(
-					"on_runtime_upgrade[{:#?}]=> Migration did not executed. This probably should be removed",
-					line!(),
-				);
-				T::DbWeight::get().reads(1)
-			}
-		}
-
-		#[cfg(feature = "try-runtime")]
-		fn pre_upgrade() -> Result<(), &'static str> {
-			use frame_support::storage::migration::get_storage_value;
+			<StorageVersion<T>>::put(crate::Releases::V1);
 
 			log::info!(
-				"pre_upgrade[{:#?}]=> with current storage version {:?} / onchain {:?}",
+				"on_runtime_upgrade[{:#?}]=> Removed Validators, Migrated to storage version {:?}",
 				line!(),
-				crate::Releases::V2_0_21,
-				<StorageVersion<T>>::get(),
+				<StorageVersion<T>>::get()
 			);
 
-			if <StorageVersion<T>>::get() == Releases::V0_0_0Legacy {
-				let pallet_prefix: &[u8] = b"Poa";
-				let storage_item_prefix: &[u8] = b"Validators";
-
-				let maybe_stored_data = get_storage_value::<Vec<T::AccountId>>(pallet_prefix, storage_item_prefix, &[]);
-
-				if let Some(stored_data) = maybe_stored_data {
-					log::info!(
-						"pre_upgrade[{:#?}]=> Validators count :: [{:#?}]",
-						line!(),
-						stored_data.len(),
-					);
-				} else {
-					log::info!(
-						"pre_upgrade[{:#?}]=> Validators is_none :: [{:#?}]",
-						line!(),
-						maybe_stored_data.is_none(),
-					);
-				}
-			} else {
-				log::info!(
-					"pre_upgrade[{:#?}]=> Migration did not executed. This probably should be removed",
-					line!(),
-				);
-			}
-
-			Ok(())
-		}
-
-		#[cfg(feature = "try-runtime")]
-		fn post_upgrade() -> Result<(), &'static str> {
-			use frame_support::storage::migration::get_storage_value;
+			T::DbWeight::get().reads_writes(1, 1)
+		} else {
 			log::info!(
-				"post_upgrade[{:#?}]=> with current storage version {:?} / onchain {:?}",
+				"on_runtime_upgrade[{:#?}]=> Migration did not executed. This probably should be removed",
 				line!(),
-				crate::Releases::V2_0_21,
-				<StorageVersion<T>>::get(),
 			);
+			T::DbWeight::get().reads(1)
+		}
+	}
 
-			if <StorageVersion<T>>::get() == Releases::V2_0_21 {
-				let pallet_prefix: &[u8] = b"Poa";
-				let storage_item_prefix: &[u8] = b"Validators";
+	#[cfg(feature = "try-runtime")]
+	pub fn pre_upgrade<T: Config>() -> Result<(), &'static str> {
+		use frame_support::storage::migration::get_storage_value;
 
-				assert!(get_storage_value::<Vec<T::AccountId>>(pallet_prefix, storage_item_prefix, &[]).is_none());
-			} else {
+		log::info!(
+			"pre_upgrade[{:#?}]=> with current storage version {:?} / onchain {:?}",
+			line!(),
+			crate::Releases::V1,
+			<StorageVersion<T>>::get(),
+		);
+
+		if <StorageVersion<T>>::get() == Releases::V0 {
+			let pallet_prefix: &[u8] = b"Poa";
+			let storage_item_prefix: &[u8] = b"Validators";
+
+			let maybe_stored_data = get_storage_value::<Vec<T::AccountId>>(pallet_prefix, storage_item_prefix, &[]);
+
+			if let Some(stored_data) = maybe_stored_data {
 				log::info!(
-					"post_upgrade[{:#?}]=> Migration did not executed. This probably should be removed",
+					"pre_upgrade[{:#?}]=> Validators count :: [{:#?}]",
 					line!(),
+					stored_data.len(),
 				);
+			} else {
+				Err("Storage Validators not found")?;
+			}
+		} else {
+			log::info!(
+				"pre_upgrade[{:#?}]=> Migration did not executed. This probably should be removed",
+				line!(),
+			);
+		}
+
+		Ok(())
+	}
+
+	#[cfg(feature = "try-runtime")]
+	pub fn post_upgrade<T: Config>() -> Result<(), &'static str> {
+		use frame_support::storage::migration::get_storage_value;
+		log::info!(
+			"post_upgrade[{:#?}]=> with current storage version {:?} / onchain {:?}",
+			line!(),
+			crate::Releases::V1,
+			<StorageVersion<T>>::get(),
+		);
+
+		if <StorageVersion<T>>::get() == Releases::V1 {
+			let pallet_prefix: &[u8] = b"Poa";
+			let storage_item_prefix: &[u8] = b"Validators";
+
+			if get_storage_value::<Vec<T::AccountId>>(pallet_prefix, storage_item_prefix, &[]).is_some() {
+				Err("Storage Validators not removed")?;
 			}
 
-			Ok(())
+		} else {
+			log::info!(
+				"post_upgrade[{:#?}]=> Migration did not executed. This probably should be removed",
+				line!(),
+			);
 		}
+
+		Ok(())
 	}
 }
