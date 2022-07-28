0b2f01cb7af02c16c734a67c7ec7d0acca2b449c
rajesh-nodle <rajesh-nodle@users.noreply.github.com>
2022-07-28 04:55:27 +0000

    deploy: 8a1b78cfac08f58ea53834ea7707066c68487828

--- implementors/frame_support/traits/members/trait.ChangeMembers.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["pallet_poa"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_poa/pallet/trait.Config.html\" title=\"trait pallet_poa::pallet::Config\">Config</a>&gt; ChangeMembers&lt;&lt;T as Config&gt;::AccountId&gt; for <a class=\"struct\" href=\"pallet_poa/pallet/struct.Pallet.html\" title=\"struct pallet_poa::pallet::Pallet\">Pallet</a>&lt;T&gt;","synthetic":false,"types":["pallet_poa::pallet::Pallet"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
