279d7a6d4ec84fe4b4dc368a75c72dbcbe48cff8
ETeissonniere <ETeissonniere@users.noreply.github.com>
2022-08-24 08:13:52 +0000

    deploy: 9080e5a2cb4fa06892e681453a2b9f473cb56386

---
+++ implementors/cumulus_pallet_dmp_queue/pallet/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["runtime_eden"] = [{"text":"impl Config for <a class=\"struct\" href=\"runtime_eden/struct.Runtime.html\" title=\"struct runtime_eden::Runtime\">Runtime</a>","synthetic":false,"types":["runtime_eden::Runtime"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
