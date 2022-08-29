767100932141894f061cd8e82c649eac7653ff7a
ETeissonniere <ETeissonniere@users.noreply.github.com>
2022-08-29 10:00:15 +0000

    deploy: 389a081e6f766f470ce519d1749e9b15eaf120a6

--- source-files.js
+++ source-files.js
@@ -1,9 +1,9 @@
 var sourcesIndex = {};
 sourcesIndex["nodle_parachain"] = {"name":"","files":["chain_spec.rs","cli.rs","command.rs","main.rs","rpc.rs","service.rs"]};
-sourcesIndex["pallet_allocations"] = {"name":"","files":["lib.rs","migrations.rs","weights.rs"]};
+sourcesIndex["pallet_allocations"] = {"name":"","files":["lib.rs","weights.rs"]};
 sourcesIndex["pallet_grants"] = {"name":"","files":["lib.rs","weights.rs"]};
 sourcesIndex["pallet_mandate"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["pallet_poa"] = {"name":"","files":["lib.rs","migrations.rs"]};
+sourcesIndex["pallet_poa"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["pallet_reserve"] = {"name":"","files":["lib.rs","weights.rs"]};
 sourcesIndex["pallet_staking"] = {"name":"","files":["hooks.rs","lib.rs","migrations.rs","set.rs","slashing.rs","types.rs","weights.rs"]};
 sourcesIndex["primitives"] = {"name":"","files":["lib.rs"]};
