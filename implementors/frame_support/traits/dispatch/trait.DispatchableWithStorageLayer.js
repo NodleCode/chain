ddabc496943e6f7f602e05ecdc5557c6571b9147
aliXsed <aliXsed@users.noreply.github.com>
2022-09-25 21:19:54 +0000

    deploy: 88f21c57a4d647a31708b4d3e65ffa76990d3c50

--- implementors/frame_support/traits/dispatch/trait.DispatchableWithStorageLayer.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["runtime_eden"] = [{"text":"impl DispatchableWithStorageLayer for <a class=\"enum\" href=\"runtime_eden/enum.Call.html\" title=\"enum runtime_eden::Call\">Call</a>","synthetic":false,"types":["runtime_eden::Call"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,12 +1,13 @@
-var sourcesIndex = {};
-sourcesIndex["nodle_parachain"] = {"name":"","files":["chain_spec.rs","cli.rs","command.rs","main.rs","rpc.rs","service.rs"]};
-sourcesIndex["pallet_allocations"] = {"name":"","files":["lib.rs","weights.rs"]};
-sourcesIndex["pallet_grants"] = {"name":"","files":["lib.rs","weights.rs"]};
-sourcesIndex["pallet_mandate"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["pallet_poa"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["pallet_reserve"] = {"name":"","files":["lib.rs","weights.rs"]};
-sourcesIndex["pallet_staking"] = {"name":"","files":["hooks.rs","lib.rs","migrations.rs","set.rs","slashing.rs","types.rs","weights.rs"]};
-sourcesIndex["primitives"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["runtime_eden"] = {"name":"","files":["constants.rs","implementations.rs","lib.rs","pallets_consensus.rs","pallets_governance.rs","pallets_nodle.rs","pallets_parachain.rs","pallets_system.rs","pallets_util.rs","version.rs"]};
-sourcesIndex["support"] = {"name":"","files":["lib.rs"]};
+var sourcesIndex = JSON.parse('{\
+"nodle_parachain":["",[],["chain_spec.rs","cli.rs","command.rs","main.rs","rpc.rs","service.rs"]],\
+"pallet_allocations":["",[],["lib.rs","weights.rs"]],\
+"pallet_grants":["",[],["lib.rs","weights.rs"]],\
+"pallet_mandate":["",[],["lib.rs"]],\
+"pallet_poa":["",[],["lib.rs"]],\
+"pallet_reserve":["",[],["lib.rs","weights.rs"]],\
+"pallet_staking":["",[],["hooks.rs","lib.rs","migrations.rs","set.rs","slashing.rs","types.rs","weights.rs"]],\
+"primitives":["",[],["lib.rs"]],\
+"runtime_eden":["",[],["constants.rs","implementations.rs","lib.rs","pallets_consensus.rs","pallets_governance.rs","pallets_nodle.rs","pallets_parachain.rs","pallets_system.rs","pallets_util.rs","version.rs"]],\
+"support":["",[],["lib.rs"]]\
+}');
 createSourceSidebar();
