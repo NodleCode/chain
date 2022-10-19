6b88585ca64c2ffb1c7c1fdc8ef9ce6d8977603e
Fredrik Simonsson <fredrik@nodle.com>
2022-10-19 13:37:05 +0900

    Added all weights from benchmarks

---
+++ runtimes/eden/src/weights/mod.rs
@@ -0,0 +1,8 @@
+pub mod frame_system;
+pub mod pallet_balances;
+pub mod pallet_multisig;
+pub mod pallet_preimage;
+pub mod pallet_scheduler;
+pub mod pallet_timestamp;
+pub mod pallet_uniques;
+pub mod pallet_utility;
