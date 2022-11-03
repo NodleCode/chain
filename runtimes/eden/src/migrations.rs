fe106534d23a69f22e48cdac0198c00607876a16
Alex Sedighi <alex@nodle.com>
2022-11-04 11:23:15 +1300

    refactor(runtime-eden): use defined consts in migrations
    Plus fix more log targets

--- runtimes/eden/src/migrations.rs
+++ runtimes/eden/src/migrations.rs
@@ -12,28 +12,36 @@ parameter_types! {
 	pub const MaxMembers: u32 = 50;
 }
 
+const VALIDATORS_SET_MODULE: &[u8] = b"ValidatorsSet";
+const MEMBERS_ITEM: &[u8] = b"Members";
+const PRIME_ITEM: &[u8] = b"Prime";
+
+const POA_MODULE: &[u8] = b"Poa";
+const STORAGE_VERSION_ITEM: &[u8] = b"StorageVersion";
+
 pub struct MoveValidatorsSetToInvulnerables;
 impl OnRuntimeUpgrade for MoveValidatorsSetToInvulnerables {
 	fn on_runtime_upgrade() -> Weight {
 		if let Some(validators) =
-			migration::take_storage_value::<BoundedVec<AccountId, MaxMembers>>(b"ValidatorsSet", b"Members", &[])
+			migration::take_storage_value::<BoundedVec<AccountId, MaxMembers>>(VALIDATORS_SET_MODULE, MEMBERS_ITEM, &[])
 		{
 			log::info!(target: "migration", "ValidatorsSet::Members are {:?}", validators);
 
 			migration::put_storage_value(b"CollatorSelection", b"Invulnerables", &[], validators);
 
-			let clear_prime_result = migration::clear_storage_prefix(b"ValidatorsSet", b"Prime", &[], None, None);
+			let clear_prime_result =
+				migration::clear_storage_prefix(VALIDATORS_SET_MODULE, PRIME_ITEM, &[], None, None);
 			if clear_prime_result.maybe_cursor.is_none() {
-				log::info!(target: "try-runtime", "ValidatorsSet::Prime with {} unique entries is removed", clear_prime_result.unique);
+				log::info!(target: "migration", "ValidatorsSet::Prime with {} unique entries is removed", clear_prime_result.unique);
 			} else {
-				log::error!(target: "try-runtime", "Failed to remove ValidatorsSet::Prime completely");
+				log::error!(target: "migration", "Failed to remove ValidatorsSet::Prime completely");
 			}
 
-			let clear_poa_result = migration::clear_storage_prefix(b"Poa", b"StorageVersion", &[], None, None);
+			let clear_poa_result = migration::clear_storage_prefix(POA_MODULE, STORAGE_VERSION_ITEM, &[], None, None);
 			if clear_poa_result.maybe_cursor.is_none() {
-				log::info!(target: "try-runtime", "Poa::StorageVersion with {} unique entries is removed", clear_poa_result.unique);
+				log::info!(target: "migration", "Poa::StorageVersion with {} unique entries is removed", clear_poa_result.unique);
 			} else {
-				log::error!(target: "try-runtime", "Failed to remove Poa::StorageVersion completely");
+				log::error!(target: "migration", "Failed to remove Poa::StorageVersion completely");
 			}
 
 			<Runtime as frame_system::Config>::DbWeight::get().writes(4)
@@ -45,7 +53,7 @@ impl OnRuntimeUpgrade for MoveValidatorsSetToInvulnerables {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<(), &'static str> {
 		if let Some(validators) =
-			migration::get_storage_value::<BoundedVec<AccountId, MaxMembers>>(b"ValidatorsSet", b"Members", b"")
+			migration::get_storage_value::<BoundedVec<AccountId, MaxMembers>>(VALIDATORS_SET_MODULE, MEMBERS_ITEM, b"")
 		{
 			Self::set_temp_storage(validators, "ValidatorsSet::Members");
 			Ok(())
@@ -56,9 +64,9 @@ impl OnRuntimeUpgrade for MoveValidatorsSetToInvulnerables {
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade() -> Result<(), &'static str> {
-		if migration::have_storage_value(b"ValidatorsSet", b"Members", b"")
-			|| migration::have_storage_value(b"ValidatorsSet", b"Prime", b"")
-			|| migration::have_storage_value(b"Poa", b"StorageVersion", b"")
+		if migration::have_storage_value(VALIDATORS_SET_MODULE, MEMBERS_ITEM, b"")
+			|| migration::have_storage_value(VALIDATORS_SET_MODULE, PRIME_ITEM, b"")
+			|| migration::have_storage_value(POA_MODULE, STORAGE_VERSION_ITEM, b"")
 		{
 			Err("Failed to remove ValidatorsSet and/or Poa")
 		} else {
