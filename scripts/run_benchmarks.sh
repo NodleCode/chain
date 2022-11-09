69d906c30a80bd42956663b9c2281247d77b1fa8
Fredrik Simonsson <fredrik@nodle.com>
2022-11-09 23:26:49 +0900

    Add some documentation to runscript

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -19,3 +19,10 @@ cargo run --profile release \
 mv runtimes/eden/src/weights/pallet_allocations.rs pallets/allocations/src/weights.rs
 mv runtimes/eden/src/weights/pallet_grants.rs pallets/grants/src/weights.rs 
 mv runtimes/eden/src/weights/pallet_reserve.rs pallets/reserve/src/weights.rs
+
+echo "Running on gcloud server? Run:"
+echo "    git commit -v -a ; git format-patch"
+echo "And on dev machine:"
+echo "    gcloud compute scp chain-bench-012bd056:chain/0001\* . --zone=us-central1-a --tunnel-through-iap "
+echo "    git apply 0001*"
+
