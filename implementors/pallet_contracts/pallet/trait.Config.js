ecb06b973e039cbee332371b281c63a06d2067de
aliXsed <aliXsed@users.noreply.github.com>
2022-11-09 00:45:55 +0000

    deploy: 348a54affc9a6dcd0cc900dee9919c7a0ba98aa8

---
+++ implementors/pallet_contracts/pallet/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"runtime_eden":[["impl Config for <a class=\"struct\" href=\"runtime_eden/struct.Runtime.html\" title=\"struct runtime_eden::Runtime\">Runtime</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
