c77cf5c48adf58c9e35fc476df6971a97c3ff2f2
simonsso <simonsso@users.noreply.github.com>
2022-12-19 04:51:32 +0000

    deploy: ca09949bde1a49b489b22aef94c82be5b45c95fb

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"runtime_eden":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"runtime_eden/struct.Runtime.html\" title=\"struct runtime_eden::Runtime\">Runtime</a>"]]
+"runtime_eden":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"runtime_eden/struct.Runtime.html\" title=\"struct runtime_eden::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/pallet_balances/pallet/trait.Config.js
+++ implementors/pallet_balances/pallet/trait.Config.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"runtime_eden":[["impl Config&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.65.0/std/primitive.unit.html\">()</a>&gt; for <a class=\"struct\" href=\"runtime_eden/struct.Runtime.html\" title=\"struct runtime_eden::Runtime\">Runtime</a>"]]
+"runtime_eden":[["impl Config&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.66.0/std/primitive.unit.html\">()</a>&gt; for <a class=\"struct\" href=\"runtime_eden/struct.Runtime.html\" title=\"struct runtime_eden::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
