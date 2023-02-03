951ec47326bc8f66415c3983c1a6afd507e20219
hounsette <houssein@nodle.com>
2023-02-03 16:20:26 +0900

    Add ConstBool importe

--- runtimes/eden/src/pallets_util.rs
+++ runtimes/eden/src/pallets_util.rs
@@ -23,7 +23,7 @@ use crate::{
 };
 use frame_support::{
 	parameter_types,
-	traits::{AsEnsureOriginWithArg, ConstU32, EqualPrivilegeOnly, Nothing},
+	traits::{AsEnsureOriginWithArg, ConstU32, ConstBool, EqualPrivilegeOnly, Nothing},
 	weights::Weight,
 };
 use frame_system::{EnsureRoot, EnsureSigned};
