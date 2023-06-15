e662596254966a0bb7a8cf66b824e90c334866a5
Fredrik Simonsson <fredrik@nodle.com>
2023-06-15 16:58:34 +0900

    Clippy update

--- runtimes/eden/src/weights/pallet_collator_selection.rs
+++ runtimes/eden/src/weights/pallet_collator_selection.rs
@@ -193,7 +193,7 @@ impl<T: frame_system::Config> pallet_collator_selection::WeightInfo for WeightIn
 	// Proof Skipped: System Events (max_values: Some(1), max_size: None, mode: Measured)
 	/// The range of component `r` is `[1, 1000]`.
 	/// The range of component `c` is `[1, 1000]`.
-	fn new_session(r: u32, c: u32, ) -> Weight {
+	fn new_session(_r: u32, c: u32, ) -> Weight {
 		// Minimum execution time: 27_720 nanoseconds.
 		Weight::from_parts(28_450_000_u64, 0)
 			// Standard Error: 1_224_557
