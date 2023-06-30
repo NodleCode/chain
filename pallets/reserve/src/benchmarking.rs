719ab9bcb342508583f52c42b802d5e3b94b7fce
Fredrik Simonsson <fredrik@nodle.com>
2023-06-30 13:17:07 +0900

    Clippy auto fix

--- pallets/reserve/src/benchmarking.rs
+++ pallets/reserve/src/benchmarking.rs
@@ -38,7 +38,7 @@ benchmarks_instance_pallet! {
 	tip {
 		let tipper = account("caller", 0, SEED);
 		let smallest_usable_amount = T::Currency::minimum_balance().saturating_add(1u32.into());
-		let starting_amount = smallest_usable_amount.saturating_add(smallest_usable_amount).into();
+		let starting_amount = smallest_usable_amount.saturating_add(smallest_usable_amount);
 		let tip_amount = smallest_usable_amount;
 		let _ = T::Currency::make_free_balance_be(&tipper,starting_amount);
 	}: _(RawOrigin::Signed(tipper), tip_amount)
@@ -46,14 +46,14 @@ benchmarks_instance_pallet! {
 	spend {
 		let dest = account("dest", 0, SEED);
 		let smallest_usable_amount = T::Currency::minimum_balance();
-		let starting_amount = smallest_usable_amount.saturating_add(smallest_usable_amount).into();
+		let starting_amount = smallest_usable_amount.saturating_add(smallest_usable_amount);
 		let value = smallest_usable_amount;
 
 		T::Currency::make_free_balance_be(&T::PalletId::get().into_account_truncating(),starting_amount);
 
 		let call = Call::<T, I>::spend{
 			to: dest,
-			amount: value.into()
+			amount: value
 		};
 		let origin = T::ExternalOrigin::try_successful_origin().map_err(|_| BenchmarkError::Weightless)?;
 	}: { call.dispatch_bypass_filter(origin)? }
