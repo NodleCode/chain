a8c305d806d1b0841cb52c11358e9ccf6ce4c1a0
Fredrik Simonsson <fredrik@nodle.com>
2023-06-30 13:56:00 +0900

    Spellcheck

--- pallets/uniques/src/benchmarking.rs
+++ pallets/uniques/src/benchmarking.rs
@@ -35,7 +35,7 @@ use crate::Pallet as Uniques;
 const SEED: u32 = 0;
 
 fn get_config<T: Config<I>, I: 'static>() -> (T::CollectionId, T::AccountId, AccountIdLookupOf<T>) {
-	let collection_owner: T::AccountId = account("colletction_owner", 0, SEED);
+	let collection_owner: T::AccountId = account("collection_owner", 0, SEED);
 	let collection_owner_lookup = T::Lookup::unlookup(collection_owner.clone());
 	let collection_id = <T as pallet_uniques::Config<I>>::Helper::collection(0);
 	(collection_id, collection_owner, collection_owner_lookup)
