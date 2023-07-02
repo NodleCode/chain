19c0f745193d40eb66533022a1c11413dba79891
Hounsette <houssein@nodle.com>
2023-07-03 01:20:43 +0900

    migration(pallet-uniques): add preupgrade test, WIP

--- runtimes/eden/src/migrations.rs
+++ runtimes/eden/src/migrations.rs
@@ -3,8 +3,9 @@ use frame_support::{
 	migration, parameter_types,
 	traits::{GetStorageVersion, OnRuntimeUpgrade},
 	weights::Weight,
-	BoundedVec,
+	BoundedVec, Twox128,
 };
+use pallet_uniques::{CollectionDetails, CollectionMetadata};
 use primitives::{AccountId, Balance};
 
 #[cfg(feature = "try-runtime")]
@@ -21,21 +22,29 @@ const TRY_RUNTIME: &str = "try-runtime";
 pub struct MovePalletUniquesToSubstrateUniques;
 impl OnRuntimeUpgrade for MovePalletUniquesToSubstrateUniques {
 	fn on_runtime_upgrade() -> Weight {
-        //todo: replace with correct weights
+		//todo: replace with correct weights
 		let mut weight = <Runtime as frame_system::Config>::DbWeight::get().reads(4);
-        weight += <Runtime as frame_system::Config>::DbWeight::get().writes(1);
-        migration::move_pallet(b"Uniques", b"SubstrateUniques");
+		weight += <Runtime as frame_system::Config>::DbWeight::get().writes(1);
+		migration::move_pallet(b"Uniques", b"SubstrateUniques");
 		weight
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
+		let collection_owner_1 = pallet_uniques::Module::<Runtime>::collection_owner(0).unwrap();
+		let collection_details = migration::take_storage_value::<CollectionDetails<AccountId, Balance>>(
+			b"Uniques",
+			b"CollectionDetails",
+			&[], //todo hash collection_owner_1
+		)
+		.unwrap();
 
-		//todo
+		Ok(collection_details.encode())
 	}
 
 	#[cfg(feature = "try-runtime")]
-	fn post_upgrade(state: Vec<u8>) -> Result<(), &'static str> {   
-        //todo
+	fn post_upgrade(state: Vec<u8>) -> Result<(), &'static str> {
+		Ok(())
+		//todo
 	}
 }
