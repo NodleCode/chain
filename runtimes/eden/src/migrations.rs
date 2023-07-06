0739c73989316a7c2f36eafb8f25ad17800c087b
Alex Sedighi <alex@nodle.com>
2023-07-07 10:48:12 +1200

    fix(runtime-eden): correct the weight of on-runtime-upgrade

--- runtimes/eden/src/migrations.rs
+++ runtimes/eden/src/migrations.rs
@@ -13,33 +13,26 @@ use primitives::{AccountId, Balance};
 use sp_std::prelude::*;
 #[cfg(feature = "try-runtime")]
 type CollectionId = u32;
-#[cfg(feature = "try-runtime")]
-const TRY_RUNTIME: &str = "try-runtime";
+
+const NEW_UNIQUES_PALLET_NAME: &[u8] = b"SubstrateUniques";
+const OLD_UNIQUES_PALLET_NAME: &[u8] = b"Uniques";
+const UNIQUES_CLASS_PREFIX: &[u8] = b"Class";
+
 pub struct MovePalletUniquesToSubstrateUniques;
 impl OnRuntimeUpgrade for MovePalletUniquesToSubstrateUniques {
 	fn on_runtime_upgrade() -> Weight {
-		//Assuming that it takes only 1 read and 1 write to move a pallet
-		let mut weight = <Runtime as frame_system::Config>::DbWeight::get().reads(1);
-		weight += <Runtime as frame_system::Config>::DbWeight::get().writes(1);
-		migration::move_pallet(b"Uniques", b"SubstrateUniques");
-		weight
+		migration::move_pallet(OLD_UNIQUES_PALLET_NAME, NEW_UNIQUES_PALLET_NAME);
+		<Runtime as frame_system::Config>::BlockWeights::get().max_block
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
-		let iter = migration::storage_key_iter::<CollectionId, CollectionDetails<AccountId, Balance>, Blake2_128Concat>(
-			b"Uniques", b"Class",
-		);
-		let mut collection_details: Vec<(CollectionId, CollectionDetails<AccountId, Balance>)> = Vec::new();
-		for collection_detail in iter {
-			log::info!(
-				target: TRY_RUNTIME,
-				"Collection before migration: {:?}",
-				collection_detail
-			);
-			collection_details.push(collection_detail);
-		}
-
+		let collection_details = migration::storage_key_iter::<
+			CollectionId,
+			CollectionDetails<AccountId, Balance>,
+			Blake2_128Concat,
+		>(OLD_UNIQUES_PALLET_NAME, UNIQUES_CLASS_PREFIX)
+		.collect::<Vec<(CollectionId, CollectionDetails<AccountId, Balance>)>>();
 		Ok(collection_details.encode())
 	}
 
@@ -47,14 +40,13 @@ impl OnRuntimeUpgrade for MovePalletUniquesToSubstrateUniques {
 	fn post_upgrade(state: Vec<u8>) -> Result<(), &'static str> {
 		let previous_collection_details: Vec<(CollectionId, CollectionDetails<AccountId, Balance>)> =
 			Decode::decode(&mut state.as_slice()).map_err(|_| "Unable to decode previous collection details")?;
-		let mut current_collection_details = Vec::<(CollectionId, CollectionDetails<AccountId, Balance>)>::new();
-		let iter = migration::storage_key_iter::<CollectionId, CollectionDetails<AccountId, Balance>, Blake2_128Concat>(
-			b"SubstrateUniques",
-			b"Class",
-		);
-		for collection_detail in iter {
-			current_collection_details.push(collection_detail);
-		}
+
+		let current_collection_details = migration::storage_key_iter::<
+			CollectionId,
+			CollectionDetails<AccountId, Balance>,
+			Blake2_128Concat,
+		>(NEW_UNIQUES_PALLET_NAME, UNIQUES_CLASS_PREFIX)
+		.collect::<Vec<_>>();
 
 		if current_collection_details != previous_collection_details {
 			return Err("Pallet Uniques Migration: Collection details do not match");
