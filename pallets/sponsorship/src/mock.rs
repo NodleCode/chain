5e198f4c9a0459eda9cb2ce682a4cd6489bdd74f
Fredrik Simonsson <fredrik@nodle.com>
2023-10-02 15:54:37 +0900

    Import BuildStorage

--- pallets/sponsorship/src/mock.rs
+++ pallets/sponsorship/src/mock.rs
@@ -28,6 +28,7 @@ use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
+	BuildStorage,
 };
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
