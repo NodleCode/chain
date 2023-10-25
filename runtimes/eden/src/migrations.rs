9cab18982dc4ceea3f4a8e0fd2a5a27e37220a7a
Fredrik Simonsson <fredrik@nodle.com>
2023-10-25 10:59:04 +0900

    Null migration for all updated pallets.

--- runtimes/eden/src/migrations.rs
+++ runtimes/eden/src/migrations.rs
@@ -33,8 +33,18 @@ where
 	T: pallet_scheduler::Config
 		+ pallet_balances::Config
 		+ pallet_membership::Config<pallet_membership::pallet::Instance3>
-		+ pallet_collective::Config<pallet_collective::pallet::Instance1>,
-{
+		+ pallet_collective::Config<pallet_collective::pallet::Instance1>
+		+ pallet_collator_selection::Config
+		+ pallet_xcm::Config
+		+ pallet_preimage::Config
+		+ pallet_multisig::Config
+		+ pallet_contracts::Config
+
+
+
+
+
+		{
 	fn on_runtime_upgrade() -> Weight {
 		// Pallets with no data to migrate, just update storage version block
 		StorageVersion::new(4).put::<pallet_scheduler::Pallet<T>>();
@@ -42,8 +52,14 @@ where
 		StorageVersion::new(4).put::<pallet_collective::Pallet<T, pallet_collective::pallet::Instance1>>();
 
 		StorageVersion::new(1).put::<pallet_balances::Pallet<T>>();
+		StorageVersion::new(1).put::<pallet_collator_selection::Pallet<T>>();
+		StorageVersion::new(1).put::<pallet_xcm::Pallet<T>>();
+		StorageVersion::new(1).put::<pallet_preimage::Pallet<T>>();
+		StorageVersion::new(1).put::<pallet_multisig::Pallet<T>>();
+		StorageVersion::new(12).put::<pallet_contracts::Pallet<T>>();
 
-		<Runtime as frame_system::Config>::BlockWeights::get().max_block
+
+		<Runtime as frame_system::Config>::BlockWeights::get().base_block
 	}
 
 	#[cfg(feature = "try-runtime")]
