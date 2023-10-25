9e02ca7de67fb80f524b1d2310a2eb86ee726113
ETeissonniere <ETeissonniere@users.noreply.github.com>
2023-10-25 15:28:10 +0000

    deploy: 90fcc7b079c5acad8d510492da52ee9dcf4e3217

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "nodle_parachain":["",[],["chain_spec.rs","cli.rs","command.rs","main.rs","rpc.rs","service.rs"]],\
 "pallet_allocations":["",[],["lib.rs","weights.rs"]],\
 "pallet_grants":["",[],["lib.rs","weights.rs"]],\
@@ -10,4 +10,4 @@ var sourcesIndex = JSON.parse('{\
 "runtime_eden":["",[["weights",[],["frame_system.rs","mod.rs","pallet_balances.rs","pallet_collator_selection.rs","pallet_contracts.rs","pallet_membership.rs","pallet_multisig.rs","pallet_preimage.rs","pallet_scheduler.rs","pallet_timestamp.rs","pallet_uniques.rs","pallet_utility.rs","pallet_xcm.rs","pallet_xcm_benchmarks_fungible.rs","pallet_xcm_benchmarks_generic.rs"]]],["constants.rs","implementations.rs","lib.rs","pallets_consensus.rs","pallets_governance.rs","pallets_nodle.rs","pallets_parachain.rs","pallets_system.rs","pallets_util.rs","version.rs","xcm_config.rs"]],\
 "support":["",[],["lib.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
