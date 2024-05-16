2975778b0c69c3fda18d7a05277cf56b5dabd52f
simonsso <simonsso@users.noreply.github.com>
2024-05-16 04:17:28 +0000

    deploy: ab70a5d517351ae228871063864bbf58847263c9

--- trait.impl/frame_support/traits/misc/trait.VariantCount.js
+++ trait.impl/frame_support/traits/misc/trait.VariantCount.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"runtime_eden":[["impl VariantCount for <a class=\"enum\" href=\"runtime_eden/enum.RuntimeHoldReason.html\" title=\"enum runtime_eden::RuntimeHoldReason\">RuntimeHoldReason</a>"],["impl VariantCount for <a class=\"enum\" href=\"runtime_eden/enum.RuntimeFreezeReason.html\" title=\"enum runtime_eden::RuntimeFreezeReason\">RuntimeFreezeReason</a>"]]
+"runtime_eden":[["impl VariantCount for <a class=\"enum\" href=\"runtime_eden/enum.RuntimeFreezeReason.html\" title=\"enum runtime_eden::RuntimeFreezeReason\">RuntimeFreezeReason</a>"],["impl VariantCount for <a class=\"enum\" href=\"runtime_eden/enum.RuntimeHoldReason.html\" title=\"enum runtime_eden::RuntimeHoldReason\">RuntimeHoldReason</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
