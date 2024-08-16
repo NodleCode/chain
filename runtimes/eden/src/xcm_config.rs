91384f37db18c602207b561dffe9bbd1599811a4
Alex Sedighi <alex@nodle.com>
2024-08-16 15:06:24 +1200

    fix: Max message size for channel is too large

--- runtimes/eden/src/xcm_config.rs
+++ runtimes/eden/src/xcm_config.rs
@@ -214,7 +214,9 @@ impl cumulus_pallet_xcmp_queue::Config for Runtime {
 	type ChannelInfo = ParachainSystem;
 	type VersionWrapper = PolkadotXcm;
 	type MaxActiveOutboundChannels = ConstU32<128>;
-	type MaxPageSize = ConstU32<{ 1 << 16 }>;
+	// Most on-chain HRMP channels are configured to use 102400 bytes of max message size, so we
+	// need to set the page size larger than that until we reduce the channel size on-chain.
+	type MaxPageSize = ConstU32<{ 103 * 1024 }>;
 	type ControllerOrigin = EnsureRoot<AccountId>;
 	type ControllerOriginConverter = XcmOriginToTransactDispatchOrigin;
 	type WeightInfo = crate::weights::cumulus_pallet_xcmp_queue::WeightInfo<Self>;
