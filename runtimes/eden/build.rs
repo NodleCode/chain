0eb0882db179222ffe4d7db4ed4b5245471bc98a
Alex Sedighi <alex@nodle.com>
2024-08-30 11:17:06 +1200

    fix: metadata hash singed extra

--- runtimes/eden/build.rs
+++ runtimes/eden/build.rs
@@ -16,12 +16,17 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-use substrate_wasm_builder::WasmBuilder;
-
+#[cfg(all(feature = "std", not(feature = "metadata-hash")))]
 fn main() {
-	WasmBuilder::new()
-		.with_current_project()
-		.export_heap_base()
-		.import_memory()
+	substrate_wasm_builder::WasmBuilder::build_using_defaults()
+}
+
+#[cfg(all(feature = "std", feature = "metadata-hash"))]
+fn main() {
+	substrate_wasm_builder::WasmBuilder::init_with_defaults()
+		.enable_metadata_hash("NODL", 11)
 		.build()
 }
+
+#[cfg(not(feature = "std"))]
+fn main() {}
