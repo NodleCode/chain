fd032b20c90fd3b569dbb51dd4a4ac360a0abdf5
ETeissonniere <ETeissonniere@users.noreply.github.com>
2025-03-04 17:41:53 +0000

    deploy: 9196935139b73c3702cb316ed9429d5623dfe5ab

--- trait.impl/bounded_collections/trait.Get.js
+++ trait.impl/bounded_collections/trait.Get.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["runtime_eden",[["impl&lt;_I: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;BlockWeights&gt;&gt; Get&lt;_I&gt; for <a class=\"struct\" href=\"runtime_eden/constants/struct.RuntimeBlockWeights.html\" title=\"struct runtime_eden::constants::RuntimeBlockWeights\">RuntimeBlockWeights</a>"]]]]);
+    var implementors = Object.fromEntries([["runtime_eden",[["impl&lt;_I: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;BlockWeights&gt;&gt; Get&lt;_I&gt; for <a class=\"struct\" href=\"runtime_eden/constants/struct.RuntimeBlockWeights.html\" title=\"struct runtime_eden::constants::RuntimeBlockWeights\">RuntimeBlockWeights</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
